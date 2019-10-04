a9a0b7532e10872391dfa5095913ce1fcdee895b
avral <qweasd_96@mail.ru>
2019-10-04 06:19:30 +0300

    update contract

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const networks = {
 
   eos: {
     name: 'eos',
-    contract: 'wwweosswapio',
+    contract: 'eostokensdex',
 
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
 
