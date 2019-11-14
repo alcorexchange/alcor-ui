560050e6ece1b2fc6123f8f9d39be391539563ca
avral <qweasd_96@mail.ru>
2019-11-14 15:00:43 -0500

    update wtf

--- plugins/mixins.js
+++ plugins/mixins.js
@@ -8,11 +8,13 @@ Vue.mixin({
         `Since the price calculation is calculated using int64 value,
         then with increasing accuracy of quantity or price,
         the price can be a floating point period. At the moment,
-        this price format is not supported. We are sorry :( `,
+        the floating point price is not supported.
+        It can not be stored in contract RAM
+        We are sorry :(`,
 
-        'About calculation of price', {
+        'The price is floating point number', {
           closeOnClickModal: true,
-          confirmButtonText: 'Ok then..'
+          confirmButtonText: 'Ok!'
       })
     }
   }
