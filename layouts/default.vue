89d1306eab6895e18fdd7572547b25b79c9feee2
avral <qweasd_96@mail.ru>
2019-11-14 16:09:39 -0500

    whatever fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,7 +21,7 @@
     .col-lg-9.col-md-12
       h1.align-self-center.lead.mt-3
         span EOS flexible, listing free, decentralized exchange.
-        span  Create markets in one click, trade whatewer you want.
+        span  Create markets in one click, trade whatever you want.
         span Forget about any regulation.
         a(:href="'eostokensdex' | monitorAccount" target="_blank").text-success.strong.ml-2  IT WORK ONCHAIN
 
