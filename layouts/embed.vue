5e4a74db3d8493340ed357b8031d33f87bfad4da
Avral <avral@pm.me>
2020-05-06 13:46:12 +0800

    prepare pages for bloks io dex embed page

---
+++ layouts/embed.vue
@@ -0,0 +1,198 @@
+<template lang="pug">
+.embed
+  nuxt
+
+  .row.mt-3
+    .col.text-mutted
+      small
+        span.text-muted App version:
+          a(href="https://github.com/avral/alcor-ui" target="_blank").text-secondary
+            span(v-if="lastCommit.commit")  {{ lastCommit.commit.message }}
+
+  footer
+    .row
+      .col.d-flex
+        span.ml-auto Created by
+          b
+            a(href="https://avral.pro" target="_blank")  #Avral
+</template>
+
+<script>
+import { captureException } from '@sentry/browser'
+import { mapGetters } from 'vuex'
+import axios from 'axios'
+
+import config from '~/config'
+
+export default {
+  data() {
+    return {
+      netError: false,
+      lastCommit: {},
+
+      networks: [],
+      current_chain: '',
+
+      app_name: config.APP_NAME,
+
+      activeLink: null
+    }
+  },
+
+  computed: {
+    ...mapGetters(['user']),
+
+    payForUser: {
+      get () {
+        return this.$store.state.chain.payForUser
+      },
+
+      set (value) {
+        this.$store.commit('chain/setPayForUser', value)
+      }
+    }
+
+    //...mapState({
+    //  current_chain: state => state.network.name
+    //})
+
+  },
+
+  watch: {
+    $route (to, from) {
+      this.activeLink = to.path
+    }
+  },
+
+  mounted() {
+    this.activeLink = this.$route.path
+    this.$store.dispatch('checkIsMobile')
+  },
+
+  async created() {
+    this.networks = Object.values(config.networks)
+    this.current_chain = this.$store.state.network.name
+    this.getVersion()
+
+    try {
+      await this.$store.getters['api/rpc'].get_info()
+    } catch (e) {
+      this.netError = true
+      console.log('Net error', e)
+    }
+  },
+
+  methods: {
+    async logout() {
+      await this.$store.dispatch('chain/logout')
+    },
+
+    changeChain(v) {
+      // TODO Move to config: APP_DOMAIN
+      if (this.current_chain == 'eos') {
+        window.location = `https://alcor.exchange`
+        return
+      }
+      window.location = `https://${this.current_chain}.alcor.exchange`
+    },
+
+    async login() {
+      if (this.$store.state.chain.scatterConnected) {
+        const loading = this.$loading({
+          lock: true,
+          text: 'Wait for wallet'
+        })
+
+        try {
+          await this.$store.dispatch('chain/login')
+        } catch (e) {
+          captureException(e)
+          this.$notify({ title: 'Scatter login error', message: e.message, type: 'error' })
+        } finally {
+          loading.close()
+        }
+      } else {
+        this.$notify({ title: 'Scatter not found', message: 'Pleace install or unlock Scatter', type: 'info' })
+      }
+    },
+
+    async scatterConnect() {
+      try {
+        await this.$store.dispatch('chain/scatterConnect')
+
+        if (this.$store.state.chain.scatterConnected)
+          this.$notify({ title: 'Scatter', message: 'Scatter connected', type: 'success' })
+      } catch (e) {
+        this.$notify({ title: 'Scatter error', message: e.message, type: 'error' })
+      }
+    },
+
+    async getVersion() {
+      this.lastCommit = (await axios.get('https://api.github.com/repos/avral/eostokens-ui/commits/master')).data
+    }
+  }
+}
+</script>
+
+<style>
+.logo {
+  height: 2.5em;
+}
+
+.chain-select {
+  width: 90px;
+}
+
+.scatter-button {
+  position: absolute;
+  right: 0;
+  z-index: 999;
+}
+
+.logo-text {
+  font-size: 3em;
+  font-family: sans-serif;
+  font-weight: 100;
+}
+
+.el-menu-item:first-child {
+  padding-left: 0px;
+}
+
+.el-menu-item:last-child {
+  padding-right: 0px;
+}
+
+h1.lead {
+  font-size: 1.2rem;
+}
+
+footer {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    padding-right: 5px;
+}
+
+@media only screen and (max-width: 600px) {
+  .logo {
+    height: 20px;
+  }
+}
+
+.market-row {
+  display: flex;
+  align-items: center;
+
+  padding: 7px 10px;
+  border-top: outset;
+  border-width: thin;
+}
+
+@media only screen and (max-width: 600px) {
+  .el-dialog, .el-message-box, .el-notification {
+    width: 95%;
+  }
+}
+</style>
---
+++ pages/embed-trade/_id/index.vue
@@ -0,0 +1,238 @@
+<template lang="pug">
+.container-fluid.mb-5.mt-1
+  .row.mt-2
+    .col-9
+      .row
+        .col
+          .row
+            .col
+              .text.item
+                .row.trade-window(v-if="!isMobile")
+                  .col-lg-5
+                    .row
+                      .col
+                        .overflowbox.box-card.p-2
+                          .row
+                            .col-md-2.p-1.pl-4
+                              TokenImage(:src="$tokenLogo(token.symbol.name, token.contract)" height="40")
+
+                            .col-md-6
+                              .row
+                                .col
+                                  b {{ token.symbol.name }}@
+                                  a(:href="monitorAccount(token.contract )" target="_blank") {{ token.contract }}
+                              .row
+                                .col
+                                  span Volume 24H:
+                                  span.text-success  {{ stats.volume24 }}
+                            .col-md-4(v-if="isPeg")
+                              withdraw
+                          .row(v-if="isPeg")
+                            .col
+                              .p-2
+                                .text {{ this.network.pegs[this.token.str].desc }}
+                    .row.mt-2
+                      .col
+                        order-book(v-loading="loading")
+
+                    .row
+                      .col
+                        LatestDeals.mt-2
+                  .col-lg-7
+                    .row
+                      .col
+                        chart
+
+                    .row
+                      .col
+                        el-tabs.h-100
+                          el-tab-pane(label="Limit trade")
+                            LimitTrade
+
+                          el-tab-pane(label="Market trade")
+                            market-trade
+
+                // Mobile verion
+                .trade-window(v-else)
+                  .row
+                    .col
+                      .overflowbox.box-card.p-2
+                        .row
+                          .col-2.p-1.pl-4
+                            TokenImage(:src="$tokenLogo(token.symbol.name, token.contract)" height="40")
+
+                          .col-10
+                            .row
+                              .col
+                                b {{ token.symbol.name }}@
+                                a(:href="monitorAccount(token.contract )" target="_blank") {{ token.contract }}
+                            .row
+                              .col
+                                span Volume 24H:
+                                span.text-success  {{ stats.volume24 }}
+                  chart
+
+                  .text.item
+                    MobileTrade
+
+    .col-3
+      .overflowbox
+        markets
+  .row
+    .col
+      hr
+      .row
+        .col
+          my-orders(v-if="user" v-loading="loading")
+</template>
+
+<script>
+import { Name, SymbolCode } from 'eos-common'
+import { captureException } from '@sentry/browser'
+import { mapGetters, mapState } from 'vuex'
+import TokenImage from '~/components/elements/TokenImage'
+import AssetImput from '~/components/elements/AssetInput'
+
+import MarketTrade from '~/components/trade/MarketTrade'
+import LimitTrade from '~/components/trade/LimitTrade'
+import MyOrders from '~/components/trade/MyOrders'
+import OrderBook from '~/components/trade/OrderBook'
+import Markets from '~/components/trade/Markets'
+import LatestDeals from '~/components/trade/LatestDeals'
+import Chart from '~/components/trade/Chart'
+import MobileTrade from '~/components/trade/MobileTrade'
+import Withdraw from '~/components/pegs/Withdraw'
+
+export default {
+  layout: 'embed',
+
+  components: {
+    TokenImage,
+    AssetImput,
+    MarketTrade,
+    MyOrders,
+    LimitTrade,
+    OrderBook,
+    LatestDeals,
+    Chart,
+    Markets,
+    MobileTrade,
+    Withdraw
+  },
+
+  async fetch({ store, error, params }) {
+    const [symbol, contract] = params.id.split('-')
+
+    let market_id
+
+    if (contract && symbol) {
+      // If it's slug
+
+      //if (c_market) {
+      //  market_id = c_market.id
+      //} else {
+      const i128_id = new Name(contract).value.shiftLeft(64).or(new SymbolCode(symbol.toUpperCase()).raw()).toString(16)
+
+      const { rows: [market] } = await store.getters['api/rpc'].get_table_rows({
+        code: store.state.network.contract,
+        scope: store.state.network.contract,
+        table: 'markets',
+        lower_bound: `0x${i128_id}`,
+        key_type: 'i128',
+        index_position: 2,
+        limit: 1
+      })
+
+      if (!market) {
+        error(`Market ${symbol}@${contract} not found!`)
+      }
+
+      market_id = market.id
+      //}
+    } else {
+      market_id = params.id
+    }
+
+    store.commit('market/setId', market_id)
+
+    this.loading = true
+    try {
+      await Promise.all([
+        store.dispatch('market/fetchMarket'),
+        store.dispatch('market/fetchDeals')
+      ])
+    } catch (e) {
+      captureException(e)
+      return error({ message: e, statusCode: 500 })
+    } finally {
+      this.loading = false
+    }
+  },
+
+  data() {
+    return {
+      price: 0.0,
+      amount: 0.0,
+
+      no_found: false,
+      loading: false
+    }
+  },
+
+  computed: {
+    ...mapState(['network']),
+    ...mapGetters('chain', ['rpc', 'scatter']),
+    ...mapState('market', ['token', 'id', 'stats']),
+    ...mapGetters(['user']),
+
+    isPeg() {
+      return Object.keys(this.network.pegs).includes(this.token.str)
+    }
+  },
+
+  created() {
+    // Auto update orders
+    setInterval(() => { this.$store.dispatch('market/fetchOrders') }, 5000)
+  },
+
+  head() {
+    return {
+      title: `Alcor Exchange | Market ${this.token.symbol.name}`,
+
+      meta: [
+        { hid: 'description', name: 'description', content: `Trade ${this.token.str} token for EOS` }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bordered {
+  border-right: 1px solid;
+}
+
+.trade-window {
+  min-height: 650px;
+}
+
+.el-form-item {
+  margin-bottom: 0px;
+}
+
+.el-card__body {
+    padding: 10px;
+}
+
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.display-4 {
+  font-size: 2.5rem;
+  font-weight: 230;
+  line-height: 1;
+}
+</style>
