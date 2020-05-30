9c82772da86d3b9534e8e4f2cf67baa8bf98309c
Avral <avral@pm.me>
2020-05-31 00:39:40 +0300

    login buttons fix

--- components/modals/Login.vue
+++ components/modals/Login.vue
@@ -1,30 +1,35 @@
 <template lang="pug">
 .row(v-loading="loading")
   .col-auto
-    el-button(size="large" @click="login(0)").mb-2
-      img(src="~/assets/logos/scatter.svg" height="30").mr-2
-      span Scatter
+    .d-flex.flex-wrap
+      .mb-2.mr-2
+        el-button(size="large" @click="login(0)")
+          img(src="~/assets/logos/scatter.svg" height="30").mr-2
+          span Scatter
+      .mb-2.mr-2
+        el-button(size="large" @click="login(1)")
+          img(src="~/assets/logos/token_poket.png" height="30").mr-2
+          span TokenPoket
 
-    el-button(size="large" @click="login(1)").mb-2
-      img(src="~/assets/logos/token_poket.png" height="30").mr-2
-      span TokenPoket
+      .mb-2.mr-2
+        el-button(size="large" @click="login(2)")
+          img(src="~/assets/logos/anchor.svg" height="30").mr-2
+          span Anchor
 
-    el-button(size="large" @click="login(2)").mb-2
-      img(src="~/assets/logos/anchor.svg" height="30").mr-2
-      span Anchor
+      .mb-2.mr-2
+        el-button(size="large" @click="login(3)")
+          img(src="~/assets/logos/simpleos.svg" height="30").mr-2
+          span SimplEOS
 
-    el-button(size="large" @click="login(3)").mb-2
-      img(src="~/assets/logos/simpleos.svg" height="30").mr-2
-      span SimplEOS
+      .mb-2.mr-2
+        el-button(size="large" @click="login(4)")
+          img(src="~/assets/logos/keycat.svg" height="30").mr-2
+          span Keycat
 
-    el-button(size="large" @click="login(4)").mb-2
-      img(src="~/assets/logos/keycat.svg" height="30").mr-2
-      span Keycat
-
-  .col.py-1
-    el-button(size="large" @click="login('wax')" v-if="network.name == 'wax'").mb-2
-      img(src="~/assets/logos/wax.svg" height="30").mr-2
-      span Wax Cloud Wallet
+      .mb-2.mr-2
+        el-button(size="large" @click="login('wax')" v-if="network.name == 'wax'")
+          img(src="~/assets/logos/wax.svg" height="30").mr-2
+          span Wax Cloud Wallet
 </template>
 
 <script>
