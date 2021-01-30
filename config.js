c5925b0e43a3a6aa0da97116bc17fc366759b84e
Avral <avral@pm.me>
2021-01-30 11:57:50 +0300

    fix hyperion streaming

--- config.js
+++ config.js
@@ -181,8 +181,9 @@ const networks = {
     monitor: 'http://wax.bloks.io',
     monitor_params: '',
     lightapi: 'https://lightapi.eosamsterdam.net',
-    hyperion: 'https://wax.eosusa.news/',
+    //hyperion: 'https://wax.eosusa.news/',
     //hyperion: 'https://wax.pink.gg/',
+    hyperion: 'https://wax.eosrio.io',
     backEnd: 'https://alcor.exchange/api/',
 
     otc: {
