f31b69ffd07de3f9b562f5066c4e6dc145f182f4
Avral <avral@pm.me>
2021-02-19 20:26:24 +0300

    update market creation fee

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const networks = {
       precision: 4
     },
 
-    marketCreationFee: '5.0000 EOS',
+    marketCreationFee: '10.0000 EOS',
     feeAccount: 'avral.pro',
 
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
