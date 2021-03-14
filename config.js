93b6a3c4031fb5e152bcfcfdfaa96b698d3c46b9
Avral <avral@pm.me>
2021-03-15 02:29:31 +0300

    change wax fee

--- config.js
+++ config.js
@@ -170,7 +170,7 @@ const networks = {
       precision: 8
     },
 
-    marketCreationFee: '800.00000000 WAX',
+    marketCreationFee: '400.00000000 WAX',
     feeAccount: 'aw.aq.waa',
 
     chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
