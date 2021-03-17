0de378c5d470c5e38b54857d2cdacf6e4bef194b
Avral <avral@pm.me>
2021-03-17 03:43:06 +0300

    add new project folder

---
+++ core/ibc/ptokens.js
@@ -0,0 +1,15 @@
+export class pToken {
+  CONTRACTS = {
+    PETH: 'eth.pTokens'
+  }
+
+  constructor(symbol) {
+    this.symbol = symbol
+  }
+}
+
+export class IBCToken {
+  constructor(symbol) {
+    this.symbol = symbol
+  }
+}
