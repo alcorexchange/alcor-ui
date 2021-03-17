1119707450bbfb54803a77bd8d83c06d87c159a1
Avral <avral@pm.me>
2021-03-17 19:40:09 +0300

    fix proton node

--- config.js
+++ config.js
@@ -130,7 +130,7 @@ const networks = {
 
     chainId: '384da888112027f0321850a169f737c33e53b388aad48b5adace4bab97f437e0',
 
-    host: 'proton.cryptolions.io',
+    host: 'proton.greymass.com',
     port: 443,
     protocol: 'https',
     monitor: 'http://proton.bloks.io',
