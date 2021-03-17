df60ba664b0cdde54e0be7932318517c59fa388e
Avral <avral@pm.me>
2021-03-17 05:32:32 +0300

    some fixes

--- server/updaters/index.js
+++ server/updaters/index.js
@@ -33,7 +33,7 @@ export async function updater(chain, app, provider, services) {
   await getSettings(network)
 
   if (services.includes('markets')) {
-    setTimeout(() => updateMarkets(network), 1000)
+    setTimeout(() => updateMarkets(network), 5000)
     setInterval(() => updateMarkets(network), 5 * 60 * 1000)
 
     streamer(network, app, network.contract, newMatch, config.CONTRACT_ACTIONS)
