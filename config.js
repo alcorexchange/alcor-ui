4856d79519a9a586089192343ba23b9fa6d2f30c
Avral <avral@pm.me>
2021-04-07 16:48:02 +0300

    upd

--- config.js
+++ config.js
@@ -176,7 +176,6 @@ const networks = {
     chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
 
     host: 'wax.greymass.com',
-    //host: 'wax.eosdsp.com',
     port: 443,
     protocol: 'https',
     monitor: 'http://wax.bloks.io',
