d45ddf91af259832e594c82975b2a3fe0a8a3e42
Avral <avral@pm.me>
2021-04-20 22:06:26 +0300

    remove dot

--- components/CircleLoadingSvg.vue
+++ components/CircleLoadingSvg.vue
@@ -22,18 +22,6 @@
         <stop offset="100%" stop-color="#42d179" stop-opacity="0" />
       </linearGradient>
       <!-- </circle> -->
-      <svg
-        class="spinner-dot dot"
-        width="5px"
-        height="5px"
-        viewBox="0 0 66 66"
-        xmlns="http://www.w3.org/2000/svg"
-        x="37"
-        y="1.5"
-      >
-        <circle class="path" fill="#42d179" cx="33" cy="33" r="30" />
-        <!-- </circle> -->
-      </svg>
     </svg>
   </section>
 </template>
