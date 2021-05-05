a96e29d7b10ec11060b101181bfe42e344570db7
Avral <avral@pm.me>
2021-05-05 10:07:09 +0300

    add bloks link for account balance click

--- components/layout/ConnectNav.vue
+++ components/layout/ConnectNav.vue
@@ -16,7 +16,7 @@
             span.ml-2(v-else) {{ network.desc }}
   .right
     .user-detail(v-if='user')
-      .balance {{ systemBalance }}
+      .balance(@click="openInNewTab(monitorAccount(user.name))") {{ systemBalance }}
       el-dropdown
         .user-name {{ user.name }}
         //template(#dropdown='')
