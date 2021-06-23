303eb61d69102a15c95653da69f11029e02444ae
avral <Avral>
2021-06-23 23:37:36 +0300

    update telos node

--- config.js
+++ config.js
@@ -228,7 +228,8 @@ const networks = {
 
     chainId: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
 
-    host: 'telos.greymass.com',
+    //host: 'telos.greymass.com',
+    host: 'telos.caleos.io',
     port: 443,
     protocol: 'https',
     monitor: 'http://telos.bloks.io',
