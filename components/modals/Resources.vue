cb71c90305d153a6fb09131c4b2913980a2c110a
Saeed <bwsaeed8@gmail.com>
2021-06-26 07:42:44 -0700

    + resources modal

---
+++ components/modals/Resources.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+el-dialog(
+  title='Network Resources',
+  :visible='isActive',
+  @close='close',
+  width='50%',
+  :append-to-body='true'
+)
+    div
+        .title Need resources? Stake WAXP
+        .desc Stake CPU and NET to vote and earn rewards. RAM is used for storing data on the blockchain.
+        .progresses
+            .progress-continaer
+                el-progress(:percentage="40" :width="100" type="circle")
+                .details
+                    .title CPU
+                    .total Total Staked: 10 WAX
+            .progress-continaer
+                el-progress(:percentage="40" :width="100" type="circle")
+                .details
+                    .title CPU
+                    .total Total Staked: 10 WAX
+</template>
+
+<script>
+import { mapMutations, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      isActive: 'resources/isActive'
+    })
+  },
+
+  methods: {
+    ...mapMutations({
+      close: 'resources/CLOSE'
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-weight: bold;
+  text-align: center;
+  font-size: 1.2rem;
+}
+.desc {
+  text-align: center;
+}
+.progresses {
+  display: flex;
+  justify-content: center;
+}
+.progress-continaer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 8px;
+  > * {
+    margin-top: 4px;
+  }
+}
+</style>
