579b8a7a377c1ae700d914e758bfa25c69a1d8b0
Saeed <bwsaeed8@gmail.com>
2021-07-13 11:31:18 -0700

    + Fee rate popup Desktop

---
+++ components/trade/FeeRate.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  el-dropdown(placement="top").fee-rate
+    AlcorButton(flat) Fee Rate
+    el-dropdown-menu(slot="dropdown").dropdown-content
+        .item
+          span.title.muted Maker Fee
+          span.value 10%
+        .h-spacer
+        .item
+          span.title.muted Taker Fee
+          span.value 10%
+</template>
+
+<script>
+import AlcorButton from '@/components/AlcorButton.vue'
+export default {
+  name: 'FeeRate',
+  components: {
+    AlcorButton
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.dropdown-content {
+  display: flex;
+  padding: 8px;
+}
+.item {
+  display: flex;
+  align-items: center;
+  .title {
+    padding-right: 2px;
+  }
+}
+.h-spacer {
+  width: 1px;
+  background: var(--background-color-secondary);
+  margin: 0 8px;
+}
+</style>
