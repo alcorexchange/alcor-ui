b196dcf1838f6c39f8cbfd3034ad896d5e8126ae
Saeed <bwsaeed8@gmail.com>
2021-08-07 05:37:15 -0700

    + wallet new index layout

---
+++ components/wallet/WalletName.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wallet-name">
+    <i class="el-icon-wallet"></i>
+    <div class="name">
+      aw.aq.waa
+    </div>
+    <div class="actions">
+      <i class="el-icon-copy-document"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WalletName'
+}
+</script>
+
+<style scoped lang="scss">
+.wallet-name {
+  display: flex;
+  align-items: center;
+  .name {
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+  > * {
+    padding: 4px;
+  }
+}
+</style>
---
+++ components/wallet/WalletTabBar.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  div.wallet-tab-bar
+    AlcorLink.tab-bar-item(v-for="{name, to} in urls" :to="to") {{name}}
+</template>
+
+<script>
+import AlcorLink from '../AlcorLink.vue'
+export default {
+  components: { AlcorLink },
+  name: 'WalletTabBar',
+  data: () => ({
+    urls: [
+      { name: 'Tokens', to: '/wallet_new' },
+      { name: 'Positions', to: '/wallet_new/positions' },
+      { name: 'Transactions', to: '/wallet_new/transactions' },
+      { name: 'NFT’s', to: '/wallet_new/nfts', isNFT: true },
+      { name: 'Liquidity Pools', to: '/wallet_new/liquidity_pools' },
+      { name: 'Resources', to: '/wallet_new/resourses' }
+    ]
+  })
+}
+</script>
+
+<style scoped lang="scss">
+.wallet-tab-bar {
+  display: flex;
+}
+.tab-bar-item {
+  flex: 1;
+  border-radius: 8px;
+  padding: 12px;
+  margin: 0 8px;
+  &:first-child {
+    margin-left: 0;
+  }
+  &:last-child {
+    margin-right: 0;
+  }
+  &.active {
+    background: var(--hover);
+  }
+}
+</style>
