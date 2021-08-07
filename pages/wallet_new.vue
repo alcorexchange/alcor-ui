c3e6eaabb77d034e52f568d9fd7c15661862aaca
Saeed <bwsaeed8@gmail.com>
2021-08-07 05:37:15 -0700

    + wallet new index layout

---
+++ pages/wallet_new.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wallet-layout">
+    <WalletName />
+    <SSpacer high />
+    <WalletHeader />
+    <SSpacer high />
+    <SSpacer high />
+    <WalletTabBar />
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+import WalletName from '~/components/wallet/WalletName.vue'
+import WalletHeader from '~/components/wallet/WalletHeader.vue'
+import SSpacer from '~/components/SSpacer.vue'
+import WalletTabBar from '~/components/wallet/WalletTabBar.vue'
+export default {
+  components: { WalletName, WalletHeader, SSpacer, WalletTabBar }
+}
+</script>
+
+<style scoped lang="scss">
+.wallet-layout {
+  padding-top: 25px;
+}
+</style>
