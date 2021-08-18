c85da54ec157f144d2a0bccb8a36844021eb8865
Saeed <bwsaeed8@gmail.com>
2021-08-17 17:15:05 -0700

    + resources page

---
+++ components/wallet/StakeAction.vue
@@ -0,0 +1,102 @@
+<template lang="pug">
+.el-card.resource-page-card
+    .header
+        i.el-icon-present
+        span.text {{renderType}}
+    .main
+      .input-label
+        .dot.green
+        span CPU {{renderType}} Amount
+      el-input
+      .percentages-container
+        AlcorButton 25%
+        AlcorButton 50%
+        AlcorButton 75%
+        AlcorButton 100%
+      .divider
+      .input-label
+        .dot.red
+        span NET {{renderType}} Amount
+      el-input
+      .percentages-container
+        AlcorButton 25%
+        AlcorButton 50%
+        AlcorButton 75%
+        AlcorButton 100%
+      SSpacer(:high="true")
+      .submit
+        AlcorButton {{renderType}}
+</template>
+
+<script>
+import AlcorButton from '@/components/AlcorButton.vue'
+import SSpacer from '@/components/SSpacer.vue'
+export default {
+  name: 'RewardsCard',
+  components: {
+    AlcorButton,
+    SSpacer
+  },
+  props: ['isStake'],
+  computed: {
+    renderType() {
+      return this.isStake ? 'Stake' : 'Unstake'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.resource-page-card {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+.input-label {
+  display: flex;
+  align-items: center;
+  .dot {
+    width: 8px;
+    height: 8px;
+    border-radius: 8px;
+    margin-right: 4px;
+    &.red {
+      background: var(--main-red);
+    }
+    &.green {
+      background: var(--main-green);
+    }
+  }
+}
+.divider {
+  margin: 8px 0;
+  height: 2px;
+  width: 100%;
+  background: var(--background-color-base);
+}
+.el-input {
+  margin: 4px 0;
+}
+.percentages-container {
+  display: flex;
+  .alcor-button {
+    flex: 1;
+    margin: 4px;
+    font-size: 0.9rem;
+    padding: 4px;
+    &:first-child {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+.submit {
+  .alcor-button {
+    width: 100%;
+    border-radius: 10px;
+    padding: 14px 10px;
+  }
+}
+</style>
---
+++ pages/wallet_new/resources.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+.resources-page
+  .resources-container
+    .resource-item-container
+      ResourceItem
+    .resource-item-container
+      ResourceItem
+    .resource-item-container
+      ResourceItem
+  .rewards-and-actions
+    .rewards-card
+      RewardsCard
+    .actions-container
+      .action-item
+        StakeAction(:isStake="true")
+      .action-item
+        StakeAction
+  SSpacer(:high="true")
+  Validators
+</div>
+</template>
+
+<script>
+// import { mapGetters, mapState } from 'vuex'
+import ResourceItem from '@/components/wallet/ResourceItem.vue'
+import RewardsCard from '@/components/wallet/RewardsCard.vue'
+import StakeAction from '@/components/wallet/StakeAction.vue'
+import Validators from '@/components/wallet/Validators.vue'
+import SSpacer from '@/components/SSpacer.vue'
+export default {
+  name: 'NFTs',
+  components: {
+    ResourceItem,
+    RewardsCard,
+    StakeAction,
+    Validators,
+    SSpacer
+  },
+  data: () => ({
+    search: '',
+  }),
+  computed: {},
+}
+</script>
+
+<style scoped lang="scss">
+.resources-container, .rewards-and-actions, .actions-container{
+  display: flex;
+  flex-wrap: wrap;
+}
+.resource-item-container{
+  padding: 20px;
+  flex: 1;
+  &:first-child {
+    padding-left: 0;
+  }
+  &:last-child {
+    padding-right: 0;
+  }
+}
+.rewards-card{
+  width: 33.3333%;
+}
+.actions-container{
+  flex: 1;
+}
+.action-item{
+  width: 50%;
+}
+.rewards-card{
+  padding: 20px;
+  padding-left: 0;
+}
+.action-item{
+  padding: 20px;
+  &:last-child {
+    padding-right: 0;
+  }
+}
+</style>
