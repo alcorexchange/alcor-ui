2d81714132cac12ed240861624aac9445709f498
sa8ab <bwsaeed8@gmail.com>
2021-11-10 05:17:33 -0800

    + partners section

--- components/landing/Partners.vue
+++ components/landing/Partners.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 .partners-section.alcor-inner
     SectionTitle.section-title Partners & API Providers
+    .items
+      a.item(v-for="{image, url, padding} in items" href="url" target="_blank" :style="{padding}")
+        img(:src="image")
 </template>
 
 <script>
@@ -13,12 +16,34 @@ export default {
   data: () => ({
     items: [
       {
-        image: require('@/assets/images/partner-graymass.png'),
+        image: require('@/assets/images/partner-greymass.png'),
         url: 'https://greymass.com/en/'
+      },
+      {
+        image: require('@/assets/images/partner-eos-amesterdam.png'),
+        url: 'https://eosamsterdam.net/',
+        padding: '40px'
       }
     ]
   })
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.section-title {
+  padding-top: 20px;
+}
+.items {
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+  .item {
+    display: flex;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+}
+</style>
