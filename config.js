78b5bf08cccc424f0cb2d41b4846230d557d1aa5
Avral <avral@pm.me>
2021-11-21 12:51:27 +0300

    update fees for market creation

--- config.js
+++ config.js
@@ -186,7 +186,7 @@ const networks = {
       precision: 8
     },
 
-    marketCreationFee: '700.00000000 WAX',
+    marketCreationFee: '150.00000000 WAX',
     feeAccount: 'aw.aq.waa',
 
     chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
