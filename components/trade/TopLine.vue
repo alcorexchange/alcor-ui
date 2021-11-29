62c294ab01b3b334b4bd0dfe5fd89169b1db7309
Avral <avral@pm.me>
2021-11-29 11:41:41 +0300

    add fav market icon

--- components/trade/TopLine.vue
+++ components/trade/TopLine.vue
@@ -1,63 +1,64 @@
 <template lang="pug">
-.box-card
-  .grid-card(v-if="!isMobile")
-    TokenImage(:src="$tokenLogo(quote_token.symbol.name, quote_token.contract)" height="30")
-    .d-flex.title-token
-      p
-        <b>{{ quote_token.symbol.name }}</b>
-      a(
-        :href="monitorAccount(quote_token.contract )"
-        target="_blank"
-        class="text-muted"
-      ) {{ quote_token.contract }}
-      p / {{ base_token.symbol.name }}
+.box-card.pl-2.pt-2
+  .row
+    .col
+      .d-flex.align-items-center(v-if="!isMobile").desktop
+        TokenImage(:src="$tokenLogo(quote_token.symbol.name, quote_token.contract)" height="30").ml-1
 
-    .d-flex(v-if="hasWithdraw")
-      // TODO Token prop & mobile version
-      Withdraw(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}")
+        .d-flex.ml-2.symbol
+          b {{ quote_token.symbol.name }}
+          a(:href="monitorAccount(quote_token.contract )" target="_blank").text-muted.ml-2 {{ quote_token.contract }}
+          span.ml-1 /  {{ base_token.symbol.name }}
 
-    //.d-flex.ml-3(v-if="$store.state.ibcTokens.includes(quote_token.contract)")
-      // TODO Token prop & mobile version
-      BOSIbc(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}")
+        //.d-flex.ml-3(v-if="hasWithdraw")
+          // TODO Token prop & mobile version
+          Withdraw(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}")
 
-    //.d-flex.ml-3.w-100.justify-content-around.desctop
-    .d-flex.align-items-center.small.change
-      span Change 24H:
-      change-percent(:change="stats.change24")
+        //.d-flex.ml-3(v-if="$store.state.ibcTokens.includes(quote_token.contract)")
+          // TODO Token prop & mobile version
+          BOSIbc(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}")
 
-      span Volume 24H:
-      span.text-success {{ stats.volume24.toFixed(2) }} {{ base_token.symbol.name }}
-  div(v-else)
-    .overflowbox.items
-      .row.align-items-center(v-if="base_token.symbol.name == network.baseToken.symbol && base_token.contract == network.baseToken.contract")
-        .col-2
-          TokenImage(:src="$tokenLogo(quote_token.symbol.name, quote_token.contract)" height="30").ml-2
-        .col-10
-          .d-flex.ml-2
-            b {{ quote_token.symbol.name }}@
-            a(:href="monitorAccount(quote_token.contract )" target="_blank") {{ quote_token.contract }}
+        //.d-flex.ml-3.w-100.justify-content-around.desctop
+        .d-flex.align-items-center.ml-3.small.text-muted
+          span Change 24H:
+          change-percent(:change="stats.change24").ml-2
 
-      .row.align-items-center(v-else)
-        .col-2
-          TokenImage(:src="$tokenLogo(base_token.symbol.name, base_token.contract)" height="30").ml-2
-        .col-10
-          .d-flex.ml-2
-            b {{ base_token.symbol.name }}@
-            a(:href="monitorAccount(base_token.contract )" target="_blank") {{ base_token.contract }}
-      .row
-        .col
-          .d-flex.ml-2
-            span Change 24H:
-            change-percent(:change="stats.change24").ml-2
-      .row
-        .col
-          .d-flex.ml-2
-            span Volume 24H:
-            span.text-success.ml-1  {{ stats.volume24.toFixed(2) }} {{ base_token.symbol.name }}
-      .row
-        .col.ml-3
-          Withdraw(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}" v-if="hasWithdraw")
-          //BOSIbc(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}" v-if="quote_token.contract == 'bosibc.io'")
+          span.ml-2 Volume 24H:
+          span.text-success.ml-2  {{ stats.volume24.toFixed(2) }} {{ base_token.symbol.name }}
+          .pointer.ml-2
+            i(:class="{ 'el-icon-star-on': isFavorite }" @click="toggleFav").el-icon-star-off
+            //i.el-icon-star-off.ml-2
+      div(v-else)
+        .overflowbox.items
+          .row.align-items-center(v-if="base_token.symbol.name == network.baseToken.symbol && base_token.contract == network.baseToken.contract")
+            .col-2
+              TokenImage(:src="$tokenLogo(quote_token.symbol.name, quote_token.contract)" height="30").ml-2
+            .col-10
+              .d-flex.ml-2
+                b {{ quote_token.symbol.name }}@
+                a(:href="monitorAccount(quote_token.contract )" target="_blank") {{ quote_token.contract }}
+
+          .row.align-items-center(v-else)
+            .col-2
+              TokenImage(:src="$tokenLogo(base_token.symbol.name, base_token.contract)" height="30").ml-2
+            .col-10
+              .d-flex.ml-2
+                b {{ base_token.symbol.name }}@
+                a(:href="monitorAccount(base_token.contract )" target="_blank") {{ base_token.contract }}
+          .row
+            .col
+              .d-flex.ml-2
+                span Change 24H:
+                change-percent(:change="stats.change24").ml-2
+          .row
+            .col
+              .d-flex.ml-2
+                span Volume 24H:
+                span.text-success.ml-1  {{ stats.volume24.toFixed(2) }} {{ base_token.symbol.name }}
+          .row
+            .col.ml-3
+              Withdraw(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}" v-if="hasWithdraw")
+              //BOSIbc(:token="{contract: quote_token.contract, symbol: quote_token.symbol.name, precision: quote_token.symbol.precision}" v-if="quote_token.contract == 'bosibc.io'")
 
 </template>
 
@@ -72,54 +73,45 @@ export default {
   components: {
     TokenImage,
     ChangePercent,
-    Withdraw
+    Withdraw,
     //BOSIbc
   },
 
   computed: {
     ...mapState(['network']),
-    ...mapState('market', ['stats', 'base_token', 'quote_token']),
+    ...mapState('market', ['stats', 'base_token', 'quote_token', 'id']),
+    ...mapState('settings', ['favMarkets']),
 
     hasWithdraw() {
       return Object.keys(this.network.withdraw).includes(this.quote_token.str)
+    },
+
+    isFavorite() {
+      return this.favMarkets.includes(this.id)
+    }
+  },
+
+  methods: {
+    toggleFav() {
+      if (this.isFavorite) {
+        this.$store.commit('settings/setFavMarkets', this.favMarkets.filter(m => m != this.id))
+      } else {
+        this.$store.commit('settings/setFavMarkets', this.favMarkets.concat([this.id]))
+      }
     }
   }
 }
 </script>
 
-<style lang="scss">
-.grid-card {
-  display: grid;
-  grid-template-columns: 35px auto 1fr;
-  grid-gap: 1rem;
-  align-items: center;
-  padding: .5rem 1rem;
-  .title-token, .change {
-    column-gap: .5rem;
-  }
-  .title-token {
-    p {
-      margin: 0;
-    }
+<style scoped lang="scss">
+.desktop {
+  font-size: 14px;
+
+  i {
+    font-size: 17px;
   }
 }
-@media screen and (min-width: 1001px) and (max-width: 1200px) {
-  .grid-card {
-    grid-template-columns: 35px 1fr;
-    grid-gap: .2rem .5rem;
-    img {
-      grid-row: 1/3;
-    }
-    .title-token {
-      grid-row: 1;
-    }
-  }
-}
-@media screen and (max-width: 1000px) {
-  .box-card {
-    margin: 10px 5px;
-  }
-}
+
 .desctop span {
   font-size: 10px;
   padding-right: 10px;
