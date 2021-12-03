62505d9946de526698c8e5d433522981e989dc67
Avral <avral@pm.me>
2021-12-03 18:16:05 +0300

    fix custom alcor-tabs imports

--- components/alcor-element/tabs/tab-nav.vue
+++ components/alcor-element/tabs/tab-nav.vue
@@ -1,7 +1,7 @@
 <script>
 /* eslint-disable */
   import TabBar from './tab-bar';
-  import { addResizeListener, removeResizeListener } from 'element-ui/src/utils/resize-event';
+  import { addResizeListener, removeResizeListener } from 'element-ui/lib/utils/resize-event';
 
   function noop() {}
   const firstUpperCase = str => {
