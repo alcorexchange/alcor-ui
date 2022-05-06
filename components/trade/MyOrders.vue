a67bdaa69707c0beae83550204f0a9f67b5d8c4f
Avral <avral@pm.me>
2022-05-06 16:46:55 +0800

    fix strange bug with thin screen for my orders

--- components/trade/MyOrders.vue
+++ components/trade/MyOrders.vue
@@ -1,33 +1,63 @@
 <template lang="pug">
 
-//el-table.w-100.my-orders(:data='orders', max-height='260' empty-text='No open orders')
-el-table.my-orders(:data='filledPositions' empty-text='No open orders')
+// MOBILE
+el-table.my-orders(:data='filledPositions' empty-text='No open orders' v-if="isMobile")
   template(slot="empty")
     span(v-if="user") No open orders
     el-button(v-else type="default" @click='$store.dispatch("modal/login")') Connect Wallet
 
-  el-table-column(label='Time', width='100', v-if='!isMobile')
-    template(slot-scope='scope')
-      span {{ scope.row.timestamp | moment("MM-DD HH:mm:ss") }}
-  el-table-column(label='Pair', v-if='!isMobile' width=100)
+  el-table-column(label='Type' width="50")
     template(slot-scope='{ row }')
-      span.hoverable.pointer(:class="{ underline: id != row.market.id }" @click="setMarket(row.market)") {{ row.market_symbol }}
-  el-table-column(label='Type' :width="isMobile ? 50 : 50")
-    template.text-success(slot-scope='{ row }')
       span.green(v-if='row.type == "buy"') {{ row.type.toUpperCase() }}
       span.red(v-else) {{ row.type.toUpperCase() }}
+
   el-table-column(label='Price' width="90")
     template(slot-scope='scope')
       span {{ scope.row.unit_price | humanPrice }}
-  el-table-column(label="Amount" :width="isMobile ? 140 : ''")
+
+  el-table-column(label="Amount")
     template(slot-scope='{ row }')
       span(v-if="isMobile") {{ row.type == 'buy' ? row.ask.quantity : row.bid.quantity | commaFloat }}
       span(v-else) {{ row.type == 'buy' ? row.ask.quantity : row.bid.quantity | commaFloat }}
-  el-table-column(:label="'Total(' + base_token.symbol.name + ')'", v-if='!isMobile')
+
+  el-table-column(label='Action', align='right' width=60)
+    template(slot-scope='scope')
+      el-button(size='mini', type='text', @click='cancel(scope.row)').red Cancel
+
+
+// DESCTOP
+el-table.my-orders(:data='filledPositions' empty-text='No open orders' v-else)
+  template(slot="empty")
+    span(v-if="user") No open orders
+    el-button(v-else type="default" @click='$store.dispatch("modal/login")') Connect Wallet
+
+  el-table-column(label='Time', width='100')
+    template(slot-scope='scope')
+      span {{ scope.row.timestamp | moment("MM-DD HH:mm:ss") }}
+
+  el-table-column(label='Pair' width=100)
+    template(slot-scope='{ row }')
+      span.hoverable.pointer(:class="{ underline: id != row.market.id }" @click="setMarket(row.market)") {{ row.market_symbol }}
+
+  el-table-column(label='Type' width="50")
+    template(slot-scope='{ row }')
+      span.green(v-if='row.type == "buy"') {{ row.type.toUpperCase() }}
+      span.red(v-else) {{ row.type.toUpperCase() }}
+
+  el-table-column(label='Price' width="90")
+    template(slot-scope='scope')
+      span {{ scope.row.unit_price | humanPrice }}
+
+  el-table-column(label="Amount")
+    template(slot-scope='{ row }')
+      span(v-if="isMobile") {{ row.type == 'buy' ? row.ask.quantity : row.bid.quantity | commaFloat }}
+      span(v-else) {{ row.type == 'buy' ? row.ask.quantity : row.bid.quantity | commaFloat }}
+
+  el-table-column(:label="'Total(' + base_token.symbol.name + ')'")
     template(slot-scope='{ row }')
       span {{ row.type == 'buy' ? row.bid.quantity : row.ask.quantity }}
-  //el-table-column(label='Action', align='right' width="60")
-  el-table-column(label='Action', align='right' :width="isMobile ? 60 : 120")
+
+  el-table-column(label='Action', align='right' width="120")
     template(v-if="!isMobile" slot="header")
       span.mr-1 Action
       span(@click="cancelAll").red.pointer.hoverable (cancel all)
