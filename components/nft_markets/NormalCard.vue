2801d0ca4b6c88fda52bedc9c5b07bccf233c328
GruDev325 <seniordev1225@gmail.com>
2022-05-11 11:43:34 +0800

    Implemented price history chart in detail page and implemented  listings, auctions, sold, bought, and sets tabs. Did some bug fixes. Refactor img URL to automicHub proxy

--- components/nft_markets/NormalCard.vue
+++ components/nft_markets/NormalCard.vue
@@ -1,6 +1,21 @@
 <template lang="pug">
-.normalcard.radius10
-  header.d-flex.justify-content-between.mb-1(v-if='mode != "templates"')
+nuxt-link.normalcard.radius10(
+  v-if='mode === "sets"',
+  :to='"#sets-" + data.collection_name'
+)
+  video.main-img.radius10(v-if='videoBackground', autoplay='true', loop='true')
+    source(
+      :src='"https://resizer.atomichub.io/videos/v1/preview?ipfs=" + videoBackground.video + "&size=370&output=mp4"',
+      type='video/mp4'
+    )
+  .main-img.radius10(v-else-if='imageBackground', :style='imageBackground')
+  .main-img.radius10(v-else, :style='defaultBackground')
+  .offer-information(v-if='mode === "sets"')
+    p.wax-name.text-center.mt-3.text-white {{ cardName }}
+.normalcard.radius10(v-else)
+  header.d-flex.justify-content-between.mb-1(
+    v-if='mode != "templates" && mode != "sets" && mode != "setsList"'
+  )
     div
       img(src='~/assets/images/small_shape.svg')
       span.card-title {{ cardTitle }}
@@ -8,35 +23,77 @@
       img.ml-1(src='~/assets/images/double_arrow.svg', alt='')
       img.ml-1(src='~/assets/images/fire.svg', alt='')
       .card_number.d-flex.align-items-center.ml-1 {{ "#" + mintCount }}
-  video.main-img.radius10(v-if='videoBackground', autoplay='', loop='')
+  video.main-img.radius10(v-if='videoBackground', autoplay='true', loop='true')
     source(
-      :src='"https://ipfs.atomichub.io/ipfs/" + videoBackground.video',
+      :src='"https://resizer.atomichub.io/videos/v1/preview?ipfs=" + videoBackground.video + "&size=370&output=mp4"',
       type='video/mp4'
     )
   .main-img.radius10(v-else-if='imageBackground', :style='imageBackground')
   .main-img.radius10(v-else, :style='defaultBackground')
   .offer-information
-    .d-flex.justify-content-between
+    .d-flex.justify-content-between(v-if='mode != "sold" && mode != "bought" && mode != "setsList"')
       p(v-if='mode != "schemas"')
         | Alcorex
         img.success-icon.ml-1(src='~/assets/images/check_circle.svg', alt='')
+      p(v-if='mode === "auctions"') Last Offer
       p(
-        v-if='kindBut === "sales" || kindBut === "auctions" || kindBut === "all"'
+        v-else-if='kindBut === "sales" || kindBut === "auctions" || kindBut === "all"'
       ) price
       p(v-else-if='mode === "inventory"') Purchase price
-    .d-flex.justify-content-between
+      p(v-else-if='mode === "listings"') Listed Price
+    .d-flex.justify-content-between(
+      v-if='mode === "sold" || mode === "bought" || mode === "setsList"'
+    )
+      p.default-price {{ cardName }}
+      p {{ collectionName }}
+    .d-flex.justify-content-between(v-if='mode != "sold" && mode != "bought" && mode != "setsList"')
       p.wax-name {{ cardName }}
       p.wax-price(
         v-if='kindBut === "sales" || kindBut === "auctions" || kindBut === "all"'
       )
         | {{ new Intl.NumberFormat().format(waxPrice) }}WAX
-    .d-flex.justify-content-between
+    .d-flex.justify-content-between(v-if='mode != "sold" && mode != "bought" && mode != "setsList"')
       p(v-if='mode != "schemas"') Default
       p.default-price(
         v-if='kindBut === "sales" || kindBut === "auctions" || kindBut === "all"'
       )
         | (${{ $systemToUSD(waxPrice) }})
-  .btn-group.justify-content-between.flex-wrap
+    .mt-2(v-if='mode === "auctions"')
+      .d-flex.justify-content-between
+        p {{ endsIn }}
+        p {{ bidCount }} Bids
+    .mt-3(v-if='mode === "listings"')
+      .d-flex.justify-content-between
+        p Best offer by
+        p Last offer
+      .d-flex.justify-content-between
+        .best-offer.d-flex
+          img.success-icon.mr-2(
+            v-if='bestOffer',
+            src='~/assets/images/MatrixCheems.svg',
+            alt=''
+          )
+          p {{ bestOffer }}
+        p.wax-price {{ new Intl.NumberFormat().format(bestPrice) }}WAX
+      .d-flex.justify-content-end (${{ $systemToUSD(bestPrice) }})
+    .mt-2.sets-list-info(v-if='mode === "setsList"')
+      .d-flex.justify-content-between
+        p Owns:
+          span.ml-1 0
+        div(v-if='data.is_transferable')
+          img(src='~/assets/images/double_arrow.svg')
+          span.ml-2.fs-18 Transferable
+      .d-flex.justify-content-between
+        p Unboxed:
+          span.ml-1 420
+        div(v-if='data.is_burnable')
+          img(src='~/assets/images/fire.svg')
+          span.ml-2.fs-18 Burnable
+      p Supply:
+        span.ml-1 {{ maxSupply + " (" + supply }}
+          img.ml-1(src='~/assets/images/fire.svg', alt='')
+          span )
+  .btn-group.justify-content-between.flex-wrap.w-100(v-if='mode != "sets"')
     button.btn-fill--grey.w-50.mr10.radius6(v-if='mode == "inventory"') Sell NFT
     b-dropdown.btn-fill--green.dropdown-more.radius6.p-0(
       v-if='mode == "inventory"',
@@ -81,12 +138,18 @@
     button.btn-border--green.mr10.radius6(v-if='kindBut == "sales"') Details
     button.btn-fill--green.radius6(v-if='kindBut == "sales"') Buy
     button.btn-border--green.mr10.radius6.smaller-btn(
-      v-if='kindBut != "sales" && mode != "inventory"'
+      v-if='kindBut != "sales" && mode != "inventory" && mode != "bought" && mode != "setsList"'
     ) Details
     button.btn-fill--green.bigger-btn.radius6(v-if='kindBut == "auctions"') Make Offer
+    button.btn-border--green.w-100.radius6.mb-2(v-if='mode == "bought" || mode === "setsList"') Detail
     button.btn-fill--green.bigger-btn.radius6(
-      v-if='kindBut != "sales" && kindBut != "auctions" && mode != "inventory"'
+      v-if='kindBut != "sales" && kindBut != "auctions" && mode != "inventory" && mode != "listings" && mode != "auctions" && mode != "sold" && mode != "bought" && mode != "setsList"'
     ) Send Offer
+    button.btn-border--green.bigger-btn.radius6(v-if='mode === "setsList"') Inventory
+    button.btn-fill--green.radius6(v-if='kindBut == "sales" || mode == "setsList"') Market
+    button.btn-fill--green.bigger-btn.radius6(v-if='mode === "sold"') Market
+    button.btn-fill--green.bigger-btn.radius6(v-if='mode === "listings"') Buy
+    button.btn-fill--green.bigger-btn.radius6(v-if='mode === "auctions"') Make Offer
 </template>
 
 <script>
@@ -95,7 +158,7 @@ import defaultImg from '~/assets/images/default.png'
 
 export default {
   components: {
-    BDropdown,
+    BDropdown
   },
   props: ['data', 'price', 'kindBut', 'mode'],
 
@@ -105,10 +168,10 @@ export default {
       defaultBackground: {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-        backgroundImage: 'url(' + defaultImg + ')',
+        backgroundImage: 'url(' + defaultImg + ')'
       },
       defaultPrice: 0,
-      sellOrders: [],
+      sellOrders: []
     }
   },
   computed: {
@@ -117,9 +180,22 @@ export default {
         if (this.data.assets[0].data.video) {
           return this.data.assets[0].data
         } else return false
-      } else if (this.mode === 'templates') {
+      } else if (this.mode === 'templates' || this.mode === 'setsList') {
         if (this.data.immutable_data.video) {
           return this.data.immutable_data
+        } else return false
+      } else if (this.mode === 'inventory' || this.mode === 'sets') {
+        if (this.data.data.video) {
+          return this.data.data
+        } else return false
+      } else if (
+        this.mode === 'listings' ||
+        this.mode === 'auctions' ||
+        this.mode === 'sold' ||
+        this.mode === 'bought'
+      ) {
+        if (this.data.assets && this.data.assets[0].data.video) {
+          return this.data.assets[0].data.video
         } else return false
       } else return false
     },
@@ -136,14 +212,31 @@ export default {
               ')',
           }
         } else return false
-      } else if (this.mode === 'inventory') {
+      } else if (this.mode === 'inventory' || this.mode === 'sets') {
         if (this.data.data.img) {
           return {
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundImage: this.data.data.img.includes('https://')
               ? this.data.data.img
-              : 'url(https://ipfs.atomichub.io/ipfs/' + this.data.data.img + ')',
+              : 'url(https://ipfs.atomichub.io/ipfs/' + this.data.data.img + ')'
+          }
+        } else return false
+      } else if (
+        this.mode === 'listings' ||
+        this.mode === 'auctions' ||
+        this.mode === 'sold' ||
+        this.mode === 'bought'
+      ) {
+        if (this.data.assets && this.data.assets[0].data.img) {
+          return {
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: this.data.assets[0].data.img.includes('https://')
+              ? this.data.assets[0].data.img
+              : 'url(https://ipfs.atomichub.io/ipfs/' +
+              this.data.assets[0].data.img +
+              ')'
           }
         } else return false
       } else if (this.mode === 'assets') {
@@ -153,10 +246,10 @@ export default {
             backgroundPosition: 'center',
             backgroundImage: this.data.data.img.includes('https://')
               ? this.data.data.img
-              : 'url(https://ipfs.atomichub.io/ipfs/' + this.data.data.img + ')',
+              : 'url(https://ipfs.atomichub.io/ipfs/' + this.data.data.img + ')'
           }
         } else return false
-      } else if (this.mode === 'templates') {
+      } else if (this.mode === 'templates' || this.mode === 'setsList') {
         if (this.data.immutable_data.img) {
           return {
             backgroundSize: 'cover',
@@ -175,13 +268,15 @@ export default {
             backgroundPosition: 'center',
             backgroundImage: this.data.collection.img.includes('https://')
               ? this.data.collection.img
-              : 'url(https://ipfs.atomichub.io/ipfs/' + this.data.collection.img + ')',
+              : 'url(https://ipfs.atomichub.io/ipfs/' +
+              this.data.collection.img +
+              ')'
           }
         } else return false
       } else if (this.data.sell && this.data.sell[0].mdata) {
         return {
           backgroundSize: 'cover',
-          backgroundImage: 'url(' + this.data.sell[0].mdata.img + ')',
+          backgroundImage: 'url(' + this.data.sell[0].mdata.img + ')'
         }
       } else return false
     },
@@ -191,8 +286,14 @@ export default {
         string = this.data.assets[0].template_mint
       } else if (this.mode === 'assets' || this.mode === 'inventory') {
         string = this.data.template_mint
+      } else if (
+        this.mode === 'listings' ||
+        this.mode === 'auctions' ||
+        this.mode === 'sold' ||
+        this.mode === 'bought'
+      ) {
+        string = this.data.assets[0]?.template_mint
       } else string = this.data.id || 0
-
       if (string.length > 4) {
         return string.substr(0, 1) + '...' + string.substr(-3)
       } else {
@@ -208,29 +309,64 @@ export default {
         return this.data.collection.author
       } else if (this.mode === 'inventory') {
         return this.data.owner
+      } else if (
+        this.mode === 'listings' ||
+        this.mode === 'auctions' ||
+        this.mode === 'sold' ||
+        this.mode === 'bought'
+      ) {
+        return this.data.assets[0].owner
       } else return this.data.maker
+    },
+    bestPrice() {
+      if (this.mode === 'listings' || this.mode === 'auctions') {
+        if (this.data.price) {
+          return (
+            this.data.price.amount /
+            Math.pow(10, this.data.price.token_precision)
+          )
+        } else return 0
+      }
+      return 0
     },
     waxPrice() {
       if (this.mode === 'market') {
-        return this.data.price.amount / (10 * this.data.price.token_precision)
+        return (
+          this.data.price.amount / Math.pow(10, this.data.price.token_precision)
+        )
       } else if (this.mode === 'assets') {
         return 0
       } else if (this.mode === 'inventory') {
         if (this.data.prices && this.data.prices[0]) {
           return (
             this.data.prices[0].average /
-            (10 * this.data.prices[0].token.token_precision)
+            Math.pow(10, this.data.prices[0].token.token_precision)
+          )
+        } else return 0
+      } else if (this.mode === 'listings' || this.mode === 'auctions') {
+        if (this.data.listing_price) {
+          return (
+            this.data.listing_price /
+            Math.pow(10, this.data.price.token_precision)
           )
         } else return 0
       } else return this.data.buy.quantity.replaceAll('WAX') * 1
     },
     cardName() {
-      if (this.mode === 'market') {
+      if (
+        this.mode === 'market' ||
+        this.mode === 'listings' ||
+        this.mode === 'auctions' ||
+        this.mode === 'sold' ||
+        this.mode === 'bought'
+      ) {
         return this.data.assets[0].name
       } else if (
         this.mode === 'assets' ||
         this.mode === 'templates' ||
-        this.mode === 'inventory'
+        this.mode === 'inventory' ||
+        this.mode === 'sets' ||
+        this.mode === 'setsList'
       ) {
         return this.data.name
       } else if (this.mode === 'schemas') {
@@ -238,7 +374,55 @@ export default {
       }
       return this.data.sell[0].owner
     },
-  },
+    bestOffer() {
+      if (this.mode === 'listings' || this.mode === 'auctions') {
+        return this.data.buyer
+      }
+      return ''
+    },
+    endsIn() {
+      if (this.mode === 'auctions') {
+        let timeDiff
+        if (new Date(+this.data.end_time) - new Date() > 0) {
+          timeDiff =
+            Math.floor(
+              (new Date(+this.data.end_time) - new Date()) / 86400000
+            ) +
+            'd ' +
+            Math.floor(
+              ((new Date(+this.data.end_time) - new Date()) % 86400000) /
+              3600000
+            ) +
+            'h'
+        } else timeDiff = '0s'
+        return timeDiff
+      }
+      return ''
+    },
+    bidCount() {
+      if (this.mode === 'auctions') {
+        return this.data.bids.length
+      }
+      return 0
+    },
+    collectionName() {
+      if (this.mode === 'setsList') {
+        return this.data.collection.name
+      } else return 'Alcorex'
+    },
+    maxSupply() {
+      if (this.mode === 'setsList') {
+        return this.data.max_supply
+      }
+      return 0
+    },
+    supply() {
+      if (this.mode === 'setsList') {
+        return this.data.issued_supply
+      }
+      return 0
+    }
+  }
 }
 </script>
 
@@ -247,9 +431,25 @@ export default {
   width: 220px;
   padding: 6px 10px;
   background-color: #202021;
+  border-radius: 10px;
 
   p {
     margin: 0;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  .sets-list-info {
+    p {
+      color: #9f979a;
+      font-size: 10px;
+    }
+
+    span {
+      font-size: 10px;
+      color: white;
+    }
   }
 
   header {
@@ -303,6 +503,10 @@ export default {
   .btn-border--green:hover {
     background-color: transparent;
     color: var(--main-action-green);
+  }
+
+  .full-btn {
+    width: 100%;
   }
 
   .btn-fill--green {
@@ -407,7 +611,8 @@ export default {
     color: #f89022;
   }
 
-  .default-price {
+  .default-price,
+  .best-offer {
     color: var(--main-action-green);
   }
 
---
+++ components/wallet/WalletNFTTab.vue
@@ -0,0 +1,223 @@
+<template lang="pug">
+.d-flex.justify-content-between.tabar-container.tradeoffertab.row.mb-4.wallet-nft-tab
+  label.search-input-group.border-bottom--gray.d-flex.align-items-center
+    img.search-icon(src='~/assets/images/search.svg', alt='')
+    input.search-input.w-100(
+      type='text',
+      :value='searchValue',
+      @input='debounceSearch',
+      @focus='focusInput',
+      @blur='blurInput',
+      placeholder='Search name or address'
+    )
+    img.down-icon(src='~/assets/images/down.svg', alt='')
+  b-dropdown.filter-input-group.border-bottom--gray.d-flex.align-items-center
+    template(#button-content)
+      img.me-1(src='~/assets/images/filter.svg', alt='')
+      p.m-0 Filter
+    button.btn.btn-collection.w-100.mb-1.d-flex.align-items-center(
+      @click='() => handleCollection("")'
+    )
+      img(src='~/assets/images/default.png')
+      p.ml-1.flex-fill.text-left.collection-name All
+    button.btn.btn-collection.w-100.mb-1.d-flex.align-items-center(
+      v-for='(item, index) in collectionData',
+      :key='index',
+      @click='() => handleCollection(item.collection_name)'
+    )
+      img(v-if='item.img && item.img.includes("https://")', :src='item.img')
+      img(v-else-if='item.img', :src='"https://ipfs.io/ipfs/" + item.img')
+      img(v-else, src='~/assets/images/default.png')
+      p.ml-1.flex-fill.text-left.collection-name {{ item.name }}
+  nuxt-link.tab-btn.border-bottom--green(
+    :to='"#" + tab.slug',
+    v-for='(tab, index) in tabData',
+    :key='index',
+    v-if='currentTab === tab.slug'
+  ) {{ tab.title }}
+  nuxt-link.tab-btn.border-bottom--gray.text-white(
+    :to='"#" + tab.slug',
+    v-else='',
+    @click='handleTab(tab.slug)'
+  ) {{ tab.title }}
+</template>
+
+<style scoped lang="scss">
+.tradeoffertab {
+  .create-collection-btn {
+    float: right;
+    text-align: center;
+    padding: 19px 3px;
+    width: 220px;
+    padding: 13px;
+    color: #000;
+    font-size: 14px;
+    font-weight: 700;
+    background: #67c23a;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+
+  .tabar-container {
+    margin-bottom: 35px;
+  }
+
+  .down-icon,
+  .filter-icon,
+  .search-icon {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+  }
+
+  .tab-btn {
+    color: #bec6cb;
+  }
+
+  .border-bottom--gray {
+    border-bottom: 1px solid #333;
+  }
+
+  .border-bottom--green {
+    border-bottom: 1px solid #67c23a;
+  }
+
+  .border-bottom--cancel {
+    border-bottom: 1px solid var(--cancel);
+  }
+
+  .search-input-group,
+  .filter-input-group {
+    position: relative;
+  }
+
+  .filter-input-group {
+    width: 87px;
+
+    .btn-collection {
+      background-color: transparent;
+      height: 37px;
+      color: #bec6cb;
+      white-space: nowrap;
+      overflow: hidden;
+
+      img {
+        min-width: 35px;
+        width: 35px;
+        height: 35px;
+        object-fit: cover;
+        border-radius: 5px;
+      }
+
+      &:hover {
+        background-color: rgb(65, 65, 65);
+      }
+
+      .collection-name {
+        overflow: hidden;
+      }
+    }
+  }
+
+  .filter-input-group .search-input {
+    width: 80px !important;
+  }
+
+  .filter-input,
+  .search-input {
+    color: var(--cancel);
+  }
+
+  .tab-btn {
+    cursor: pointer;
+    padding: 12px 0;
+    font-size: 15px;
+    text-align: center;
+    width: 130px;
+  }
+
+  .search-icon {
+    left: 0;
+  }
+
+  .down-icon {
+    left: auto;
+    right: 10px;
+  }
+
+  .search-input {
+    padding: 5px 25px;
+    outline: none;
+    border: none;
+    background-color: transparent;
+  }
+}
+</style>
+<style lang="scss">
+.wallet-nft-tab {
+  .filter-input-group {
+    .dropdown-toggle {
+      height: 37px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-radius: 0;
+      padding: 5px;
+      border: 0;
+      box-shadow: none !important;
+      outline: none !important;
+      color: var(--color-text-primary);
+      background: transparent !important;
+    }
+
+    .dropdown-menu {
+      background: #333;
+      border: 1px dashed var(--main-green);
+      max-height: 400px;
+      width: 250px;
+      overflow: auto;
+    }
+  }
+}
+</style>
+<script>
+import { BDropdown } from 'bootstrap-vue'
+export default {
+  components: {
+    BDropdown,
+  },
+  props: [
+    'tabData',
+    'currentTab',
+    'handleTab',
+    'handleSearch',
+    'collectionData',
+    'handleCollection',
+    'searchValue',
+    'handleSearchValue',
+  ],
+  data() {
+    return {
+      search: null,
+      sellOrders: [],
+    }
+  },
+  methods: {
+    debounceSearch(event) {
+      this.handleSearchValue(event.target.value)
+      clearTimeout(this.debounce)
+      this.debounce = setTimeout(() => {
+        this.handleSearch(event.target.value)
+        // search function
+      }, 600)
+    },
+    focusInput(event) {
+      event.target.parentElement.classList.add('border-bottom--cancel')
+    },
+    blurInput(event) {
+      event.target.parentElement.classList.remove('border-bottom--cancel')
+    },
+  },
+}
+</script>
