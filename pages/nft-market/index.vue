1a4b8ce3a9391811691e66a2688064475ae0639f
GruDev325 <seniordev1225@gmail.com>
2022-05-13 12:37:49 +0800

    fix on total sold display symbol, double active tabs & card style issue

--- pages/nft-market/index.vue
+++ pages/nft-market/index.vue
@@ -1,197 +1,368 @@
 <template lang="pug">
-.row.mt-3
-  .col-lg-2.filters.pr-0
-    .row.mb-2
-      .col
-        NewOrder
-    .row
-      .col
-        el-card
-          div(slot="header")
-            span {{ $t('Authors') }}
-            // FIXME el-button(style="float: right;" size="mini" type="text" @click="clearAuthorFilters") CLEAR
-          el-checkbox(
-            v-for="author in authors"
-            :key="author"
-            @change="addAutorFilter(author)"
-            :checked="isAuthorCheked(author)"
-          ).w-100 {{ author }}
-
-    .row.mt-2.mb-2
-      .col
-        el-card
-          div(slot="header")
-            span {{ $t('Categories') }}
-          el-checkbox(
-            v-for="category in categories"
-            :key="category"
-            @change="addCatFilter(category)"
-            :checked="isCatCheked(category)"
-          ).w-100 {{ category }}
-
-  .col-lg-10.pr-0
-    .row
-      .col
-        .d-flex
-          el-input(v-model="search" :placeholder="$t('Search NFT') + ' : ' + $t(`ID/Name/Category/Author`)" clearable size="medium")
-
-          nuxt-link(:to="localePath('/nft-market/create', $i18n.locale)").ml-3
-            el-button(tag="el-button" icon="el-icon-plus" size="medium") {{ $t('Create NFT token') }}
-
-          nuxt-link(:to="localePath('/wallet/nfts', $i18n.locale)").ml-3
-            el-button(type="info" icon="el-icon-wallet" size="medium") {{ $t('NFT Wallet') }}
-    hr
-
-    .row
-      .col
-        el-alert(type="error" title="Beware of scammers!" show-icon)
-          p {{ $t('NFT_ALERT') }}
-
-    .row.mt-3
-      .col
-        .market-cards
-          card.item(
-            v-for="(order, i) in filteredOrders"
-            :order="order"
-            :key="order.id + i"
-          )
-
+.nft-container.j-container
+  .grid-container.row
+    .card-container.lg-6.md-12.sm-12.xl-12(
+      v-for='(card, index) in cardData',
+      :key='index'
+    )
+      NftCard(:data='card')
+    .lg-12.md-12.sm-12.xl-12.relation-item
+      NftRelation(:data='relationData', :price='getPrice')
+  #loading.d-flex.justify-content-center(v-if='!filteredOrders.length')
+    .spinner-border(role='status')
+      span.sr-only Loading...
+  h1.recent-title(v-if='filteredOrders.length') Recent Listenings
+  CatCarousel.nft-market-carousel(
+    v-if='filteredOrders.length',
+    :items='filteredOrders.slice(0, 5)',
+    :item-per-page='4',
+    :indicators-config='{ hideIndicators: true }'
+  )
+    template(slot='item', slot-scope='{ data, index }')
+      NormalCard(
+        v-if='data',
+        :data='data',
+        :price='getPrice',
+      )
+  h1.recent-title(v-if='filteredOrders.length') New NFTs
+  CatCarousel.nft-market-carousel(
+    v-if='filteredOrders.length',
+    :items='filteredOrders.reverse().slice(0, 5)',
+    :item-per-page='4',
+    :indicators-config='{ hideIndicators: true }'
+  )
+    template(slot='item', slot-scope='{ data, index }')
+      NormalCard(
+        v-if='data',
+        :data='data',
+        :price='getPrice',
+      )
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import Card from '~/components/nft_markets/Card'
-import NewOrder from '~/components/nft_markets/NewOrder'
-
+import { CatCarousel } from 'vue-cat-carousel'
+import NftCard from '~/components/nft_markets/cards/NftCard'
+import NftRelation from '~/components/nft_markets/NftRelation'
+import NormalCard from '~/components/nft_markets/cards/NormalCard'
+import Img1 from '~/assets/images/nft_marketplace.webm'
+import Img2 from '~/assets/images/wallet.webm'
+import Img3 from '~/assets/images/nft_explorer.webm'
+import Img4 from '~/assets/images/nft.webm'
+import subImg from '~/assets/icons/wax.png'
 export default {
   components: {
-    Card,
-    NewOrder
+    NftCard,
+    NftRelation,
+    NormalCard,
+    CatCarousel,
   },
-
   data() {
     return {
+      title: 'ABC',
+      relationData: '',
+      normalcardData: [
+        {
+          title: 'ALCOR',
+          subTItle: 'NFT MARKETPLACE',
+          img: Img1,
+        },
+        {
+          title: 'WALLET',
+          subTItle: 'flfum.wam',
+          img: Img2,
+          subImage: subImg,
+        },
+        {
+          title: 'ALCOR',
+          subTItle: 'NFT EXPLORER',
+          img: Img3,
+        },
+        {
+          title: 'ALCOR',
+          subTItle: 'CREATE NFT',
+          img: Img4,
+        },
+      ],
       search: '',
-
-      sellOrders: []
+      sellOrders: [],
+      settings: {
+        dots: false,
+        focusOnSelect: true,
+        infinite: true,
+        autoplay: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        touchThreshold: 5,
+        autoplaySpeed: 4600,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              arrows: false,
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              arrows: false,
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      },
     }
   },
-
   computed: {
-    ...mapState(['network']),
+    ...mapState(['network', 'user']),
     ...mapState('nft', ['orders', 'authorFilter', 'catFilter']),
-
+    ...mapState('wallet', ['systemPrice']),
+    cardData() {
+      let data = [
+        {
+          title: 'ALCOR',
+          subTItle: 'NFT MARKETPLACE',
+          img: Img1,
+          to: '/nft-market/nft-marketplace',
+        },
+        {
+          title: 'WALLET',
+          subTItle: this.user ? this.user.name : '',
+          img: Img2,
+          subImage: subImg,
+          to: '/wallet-inventory',
+        },
+        {
+          title: 'ALCOR',
+          subTItle: 'NFT EXPLORER',
+          img: Img3,
+          to: '/nft-market/nftexplorer',
+        },
+        {
+          title: 'ALCOR',
+          subTItle: 'CREATE NFT',
+          img: Img4,
+          to: '/nft-market/createnft',
+        },
+      ]
+      return data
+    },
     filteredOrders() {
       let orders = this.orders
-
-      if (this.authorFilter.length > 0) orders = orders.filter(o => {
-        return o.sell.some(s => this.authorFilter.includes(s.author))
-      })
-
-      if (this.catFilter.length > 0) orders = orders.filter(o => {
-        return o.sell.some(s => this.catFilter.includes(s.category))
-      })
-
-      orders = orders.filter(o => {
-        return o.sell.some(s => {
-          const orderSearchData = s.author + s.category + s.id + JSON.stringify(s.idata) + JSON.stringify(s.mdata)
-          return orderSearchData.toLowerCase().includes(this.search.toLowerCase())
+      if (this.authorFilter.length > 0)
+        orders = orders.filter((o) => {
+          return o.sell.some((s) => this.authorFilter.includes(s.author))
+        })
+      if (this.catFilter.length > 0)
+        orders = orders.filter((o) => {
+          return o.sell.some((s) => this.catFilter.includes(s.category))
+        })
+      orders = orders.filter((o) => {
+        return o.sell.some((s) => {
+          const orderSearchData =
+            s.author +
+            s.category +
+            s.id +
+            JSON.stringify(s.idata) +
+            JSON.stringify(s.mdata)
+          return orderSearchData
+            .toLowerCase()
+            .includes(this.search.toLowerCase())
         })
       })
-
       return orders
     },
-
     authors() {
       const authors = []
-
-      this.orders.map(o => {
-        o.sell.map(o => authors.push(o.author))
+      this.orders.map((o) => {
+        o.sell.map((o) => authors.push(o.author))
       })
-
       return Array.from(new Set(authors))
     },
-
+    getPrice() {
+      let price = this.systemPrice
+      return price
+    },
     categories() {
       const categories = []
-
-      this.orders.map(o => {
-        o.sell.map(o => categories.push(o.category))
+      this.orders.map((o) => {
+        o.sell.map((o) => categories.push(o.category))
       })
-
       return Array.from(new Set(categories))
-    }
+    },
   },
-
   mounted() {
     this.$store.dispatch('nft/fetch')
+    this.getSymbolInfo()
   },
-
   methods: {
     addAutorFilter(author) {
       if (this.authorFilter.includes(author)) {
-        this.$store.commit('nft/setAuthorFilter', this.authorFilter.filter(a => a != author))
+        this.$store.commit(
+          'nft/setAuthorFilter',
+          this.authorFilter.filter((a) => a != author)
+        )
       } else {
-        this.$store.commit('nft/setAuthorFilter', [...this.authorFilter, author])
+        this.$store.commit('nft/setAuthorFilter', [
+          ...this.authorFilter,
+          author,
+        ])
       }
     },
-
     clearAuthorFilters() {
       this.$store.commit('nft/setAuthorFilter', [])
     },
-
     isAuthorCheked(author) {
       return this.authorFilter.includes(author)
     },
-
     addCatFilter(cat) {
       if (this.catFilter.includes(cat)) {
-        this.$store.commit('nft/setCatFilter', this.catFilter.filter(a => a != cat))
+        this.$store.commit(
+          'nft/setCatFilter',
+          this.catFilter.filter((a) => a != cat)
+        )
       } else {
         this.$store.commit('nft/setCatFilter', [...this.catFilter, cat])
       }
     },
-
     clearCatFilters() {
       this.$store.commit('nft/setCatFilter', [])
     },
-
     isCatCheked(cat) {
       return this.catFilter.includes(cat)
-    }
+    },
+    async getSymbolInfo() {
+      const data = await this.$store.dispatch('api/getSymbolInfo')
+      this.relationData = data
+    },
   },
-
   head() {
     return {
-      title: `Alcor NFT Market | ${this.$t('META_NFT_MARKET_TITLE')} on ${this.network.name.toUpperCase()} chain`,
-
+      title: `Alcor NFT Market | Trustless NFT market on ${this.network.name.toUpperCase()} chain`,
       meta: [
-        { hid: 'description', name: 'description', content: this.$t('META_NFT_MARKET_DESCRIPTION') }
-      ]
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Atomic, no fee, NFT marketplace.',
+        },
+      ],
     }
-  }
+  },
 }
 </script>
 
-<style>
-.market-cards .el-card__header {
-  padding: 10px 20px;
+<style lang="scss">
+.nft-container {
+  margin-top: 30px;
 }
 
-.market-cards {
+.nft-market-carousel {
+  .cat-carousel__navigation {
+    display: block !important;
+
+    .cat-carousel__default-nav {
+      background: transparent;
+      border: 0;
+      box-shadow: none;
+      width: 17px;
+      height: 29px;
+
+      img {
+        display: none;
+      }
+    }
+
+    &.cat-carousel__navigation {
+      left: -38px;
+
+      .cat-carousel__default-nav {
+        background: url('~/assets/images/LeftArrow.svg') !important;
+        background-size: cover;
+      }
+
+      &.cat-carousel__navigation__next {
+        left: auto;
+        right: -38px;
+
+        .cat-carousel__default-nav {
+          background: url('~/assets/images/RightArrow.svg') !important;
+          background-size: cover;
+        }
+      }
+    }
+  }
+}
+
+.nft-container .grid-container {
   display: flex;
-  flex-wrap: wrap !important;
   justify-content: space-between;
+  width: calc(100% - 20px);
+  margin: auto;
 }
 
-.market-cards .item {
-  width: 32.8%;
-  margin-bottom: 10px;
+.card-container {
+  width: 440px;
+  height: 200px;
+  margin-bottom: 30px;
 }
 
-@media only screen and (max-width: 600px) {
-  .market-cards .item {
-    width: 100%;
+#loading {
+  margin-top: 20px;
+  position: relative;
+}
+
+.slick-prev:before,
+.slick-next:before {
+  content: '';
+}
+
+.slick-prev,
+.slick-prev:hover,
+.slick-next,
+.slick-next:hover {
+  background: url('~/assets/images/left_arrow.png') !important;
+  background-size: 100% 100% !important;
+  width: 12.5px !important;
+  height: 25px !important;
+}
+
+.slick-next,
+.slick-next:hover {
+  background: url('~/assets/images/right_arrow.png') !important;
+  background-size: 100% 100% !important;
+}
+
+.relation-item {
+  width: 100%;
+}
+
+.recent-title {
+  margin: 30px 0;
+  font-size: 36px;
+  font-weight: bold;
+  color: #fff;
+}
+
+@media only screen and (max-width: 1000px) {
+  .nft-container .grid-container {
+    justify-content: center;
+    width: calc(100% - 25px);
   }
 }
 </style>
