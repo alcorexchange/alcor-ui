368d322b51b0dc642b5c40c5bf6f1c90a392db03
Vlad <clay.zenx@gmail.com>
2022-05-19 10:47:43 +0700

    Meta tags

--- locales/en.js
+++ locales/en.js
@@ -1,4 +1,6 @@
 export default {
+  META_DESCRIPTION: 'Meta description',
+  META_TITLE: 'Meta title',
   'Start traiding': 'Start traiding',
   'Read docs': 'Read docs'
 }
--- locales/ru.js
+++ locales/ru.js
@@ -1,4 +1,6 @@
 export default {
+  META_DESCRIPTION: 'Мета описание',
+  META_TITLE: 'Мета заголовок',
   'Start traiding': 'Начать торговлю',
   'Read docs': 'Читать документацию'
 }
