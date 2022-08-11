cc78cb4049e0265ce4602e816aea6beaf96012a9
Avral <avral@pm.me>
2022-08-12 02:49:41 +0300

    update service config

--- server/services/UpdaterService/index.js
+++ server/services/UpdaterService/index.js
@@ -6,37 +6,20 @@ import { createClient } from 'redis'
 import { startUpdaters } from './start'
 
 async function start () {
-  // Trying to connect all service here before start
+  const redisClient = createClient()
+  await redisClient.connect()
+  console.log('Redis connected..')
 
-  while (true) {
-    try {
-      const redisClient = createClient()
-      await redisClient.connect()
-      console.log('Redis connected..')
-      break
-    } catch (e) {
-      console.error('REDIS CONNECTION ERRR, restarting..: ', e)
-      await new Promise((resolve, reject) => setTimeout(resolve, 1000))
-    }
-  }
-
-  while (true) {
-    try {
-      const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/alcor_prod_new`
-      await mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
-      console.log('MongoDB connected!')
-      break
-    } catch (e) {
-      console.error('MONGO CONNECTION ERRR, restarting..: ', e)
-      await new Promise((resolve, reject) => setTimeout(resolve, 1000))
-    }
-  }
+  const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/alcor_prod_new`
+  await mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+  console.log('MongoDB connected!')
 
   // FOR PM2
   process.send('ready')
-  process.on('SIGINT', process.exit(0))
 
   startUpdaters()
+
+  process.on('SIGINT', process.exit(0))
 }
 
 start()
