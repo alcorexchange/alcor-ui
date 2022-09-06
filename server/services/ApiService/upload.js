c70c68a32ddd0fee5d38586b5b4a13abc0a2243d
Avral <avral@pm.me>
2022-09-06 18:44:23 +0300

    update env for ipfs

--- server/services/ApiService/upload.js
+++ server/services/ApiService/upload.js
@@ -17,8 +17,8 @@ upload.post('/ipfs', formidable(), async (req, res) => {
       maxContentLength: 'Infinity', //this is needed to prevent axios from erroring out with large directories
       headers: {
         'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-        pinata_api_key: '17dc357e28a2501a3001',
-        pinata_secret_api_key: 'd6bce3a02195e4e4ec54958d7e88dec7ad6098764ca5eac16d8bac4bce639f1c'
+        pinata_api_key: process.env.IPFS_API_KEY,
+        pinata_secret_api_key: process.env.IPFS_SECRET_API_KEY
       }
     }
   )
