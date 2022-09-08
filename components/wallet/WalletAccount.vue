94d49850f4168ddbb5d02355a5a6439bbb1bfc5c
clayzenx <clay.zenx@gmail.com>
2022-09-08 09:30:00 +0700

    Disconnect btn

--- components/wallet/WalletAccount.vue
+++ components/wallet/WalletAccount.vue
@@ -4,7 +4,8 @@
     profile-image.account-photo(editable :src.sync="profileImageSrc", :hash.sync="profileImageHash" :size="40")
     wallet-name
     wallet-actions
-  .d-flex.gap-6
+  .d-flex.gap-6.align-items-center.pointer(@click="logout" v-if="user")
+    disconnect(:color="$colorMode.preference === 'dark' ? '#f2f2f2' : '#303133'" width="13" height="13")
     span Disconnect
 
 </template>
@@ -14,9 +15,10 @@ import { mapActions, mapGetters } from 'vuex'
 import ProfileImage from '~/components/ProfileImage.vue'
 import WalletName from '~/components/wallet/WalletName.vue'
 import WalletActions from '~/components/wallet/WalletActions.vue'
+import Disconnect from '~/components/svg-icons/Disconnect.vue'
 
 export default {
-  components: { ProfileImage, WalletName, WalletActions },
+  components: { ProfileImage, WalletName, WalletActions, Disconnect },
   data: () => ({
     profileImageSrc: '',
     profileImageHash: ''
@@ -37,6 +39,10 @@ export default {
   },
   methods: {
     ...mapActions('social', ['updatePhoto', 'getPhotoHash']),
+    async logout() {
+      console.log('logout')
+      await this.$store.dispatch('chain/logout')
+    },
     async setProfileImage() {
       const hash = this.user && await this.getPhotoHash()
       this.profileImageSrc = hash && `https://gateway.pinata.cloud/ipfs/${hash}`
