69e6be8d750fa2e52e7fa5915079370290b7f97c
clayzenx <clay.zenx@gmail.com>
2022-09-30 14:55:45 +0700

    AlcorTabs

--- pages/account/_id/nfts.vue
+++ pages/account/_id/nfts.vue
@@ -1,14 +1,35 @@
 <template lang="pug">
 #account-id-nfts-layout.d-flex.flex-column.gap-16.mt-2
-  account-nfts-header
+  alcor-tabs(:links="true" :tabs="tabs")
   main.account-nft-main
     nuxt-child
 
 </template>
 
 <script>
-import AccountNftsHeader from '~/components/account/AccountNftsHeader.vue'
+import AlcorTabs from '~/components/AlcorTabs'
 export default {
-  components: { AccountNftsHeader }
+  components: { AlcorTabs },
+  data: () => ({
+    filters: {
+      match: 'asd',
+      s: 'key',
+      bool: false
+    }
+  }),
+  computed: {
+    tabs() {
+      return [
+        {
+          label: 'Inventory',
+          route: {
+            path: '/swap',
+            query: this.filters
+          }
+        },
+        { label: 'Listings', href: '/account/' + this.$route.params.id + '/nfts/listings' }
+      ]
+    }
+  }
 }
 </script>
