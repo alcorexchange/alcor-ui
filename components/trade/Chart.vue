e904e22d8d98a28d1fc36b1d772fd415e381815b
Avral <avral@pm.me>
2023-01-28 15:31:19 +0300

    trying to fix chart indicators

--- components/trade/Chart.vue
+++ components/trade/Chart.vue
@@ -616,7 +616,7 @@ export default {
 
           'popup_hints',
 
-          //'save_chart_properties_to_local_storage',
+          'save_chart_properties_to_local_storage',
           //'use_localstorage_for_settings'
 
           //'header_resolutions',
