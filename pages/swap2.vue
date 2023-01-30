f2b57c15cf934ac161e5daf1d61adaa8b78b7352
clayzenx <clay.zenx@gmail.com>
2023-01-30 15:08:04 +0700

    Routes

---
+++ pages/swap2.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.d-flex.gap-32.w-100
+  alcor-container
+    alcor-tabs(:links="true" :tabs="tabs")
+  nuxt-child
+</template>
+
+<script>
+import AlcorTabs from '~/components/AlcorTabs'
+
+export default {
+  components: {
+    AlcorTabs
+  },
+  data: () => ({
+    tabs: [{
+      label: 'Swap',
+      route: { path: '/swap2/swap-tokens' }
+    },
+    {
+      label: 'Manage Liquidity',
+      route: { path: '/swap2/manage-liquidity' }
+    }]
+  })
+}
+</script>
