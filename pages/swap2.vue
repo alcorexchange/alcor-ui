2c15157e60524511e444e2152eb3318bded245f3
clayzenx <clay.zenx@gmail.com>
2023-02-01 20:12:01 +0700

    Token Select

--- pages/swap2.vue
+++ pages/swap2.vue
@@ -2,21 +2,31 @@
 .d-flex.gap-32.w-100
   alcor-container.pool-form.d-flex.flex-column.gap-32
     alcor-tabs.swap-tabs(:links="true" :tabs="tabs")
-    element-select.token-select(:options="tokens" keyProps="quote_name")
+    element-select.token-select(:options="tokens" keyField="quote_name" :disableList="true")
       template(#preOptions)
-        el-input(v-model='search' placeholder="Search by name or contract" clearable)
-      template(#option="{ option }")
-        token-option(:token="option" @click="changeSelected")
+        el-input(v-model='search' placeholder="Search by name or contract" clearable style="width: 370px")
+        recycle-scroller(:emit-update="true" class="scroller" :items="tokensFiltered" :item-size="45" keyField="symbol")
+          template(v-slot="{ item: token }")
+            .token.d-flex.justify-content-between.align-items-center.p-2
+              .d-flex.gap-8.align-items-center
+                token-image(:src="$tokenLogo(token.symbol, token.contract)" height="25")
+                .fs-16 {{ token.quote_name }}
+                .text-muted.small {{ token.contract }}
+
+              //.ml-auto(v-if="user")
+                span.text-muted {{ token.balance | commaFloat }}
+
       template(#selected)
         .d-flex.gap-8.align-items-center.px-2
-          el-input(v-model='inputAmount' :placeholder="0.0" clearable)
+          el-input(type="number" v-model='inputAmount' :placeholder="0.0" clearable)
           token-image(:src='$tokenLogo(inputToken.quote_name, inputToken.contract)')
-          .disable {{ inputToken.quote_name }}
+          .fs-18.disable {{ inputToken.quote_name }}
 
   nuxt-child
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import AlcorTabs from '~/components/AlcorTabs'
 import AlcorContainer from '~/components/AlcorContainer'
 import AlcorSelect from '~/components/AlcorSelect'
@@ -57,6 +67,30 @@ export default {
       }
     ]
   }),
+  computed: {
+    //TODO mock tokens
+    ...mapGetters({
+      tokens0: 'swap/tokens0'
+    }),
+    //mock tokens
+    tokensFiltered() {
+      return this.tokens0
+        .filter((t) => {
+          const s = (t.symbol + '@' + t.contract).toLowerCase()
+          return s.includes(this.search.toLowerCase())
+        })
+        .map((t) => {
+          t.balance = this.$tokenBalance(t.symbol || t.currency, t.contract)
+          return t
+        })
+        .sort((a, b) => {
+          return a.balance == '0.0000' ? 1 : -1
+        })
+        .sort((a, b) =>
+          parseFloat(a.balance) < parseFloat(b.balance) ? 1 : -1
+        )
+    }
+  },
   methods: {
     changeSelected(token) {
       this.inputToken = token
@@ -68,6 +102,18 @@ export default {
 <style lang="scss">
 .pool-form {
   width: 405px;
+
+  .el-dropdown-menu.element-options {
+    top: 60px;
+  }
+
+  .el-input {
+    font-size: 16px;
+  }
+
+  .token:hover {
+    background-color: red;
+  }
 
   .swap-tabs {
     gap: 0px;
