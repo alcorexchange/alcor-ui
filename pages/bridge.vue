dfc31ba1e7aa557f87acab4ea3a31c34a492f171
Avral <avral@pm.me>
2023-02-08 04:52:59 +0300

    update

--- pages/bridge.vue
+++ pages/bridge.vue
@@ -1,20 +1,24 @@
 <template lang="pug">
 #bridge-page.d-flex.align-items-center.flex-column.gap-16.mt-5
-  circles-bg.bg(stroke="#F89022")
-  circles-bg.bg2(stroke="red")
-  h1.fs-48 Alcor IBC Bridge
-  .fs-24 Bridge assets from different chains
+  .greet
+    h1.fs-48 Alcor IBC Bridge
+    .fs-24 Bridge assets from different chains
 
   client-only
     bridge-form.mt-4
+
+    circle1(color="#94591C" total="20").circle1
+    circle2(color="#462C7E" total="20").circle2
 </template>
 
 <script>
 import BridgeForm from '~/components/bridge/BridgeForm.vue'
 import CirclesBg from '~/components/bridge/CirclesBg.vue'
+import Circle1 from '~/components/bridge/Circle1.vue'
+import Circle2 from '~/components/bridge/Circle2.vue'
 
 export default {
-  components: { BridgeForm, CirclesBg },
+  components: { BridgeForm, CirclesBg, Circle1, Circle2 },
 
   head() {
     return {
@@ -33,6 +37,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.greet {
+  z-index: 100
+}
+
+.circle1 {
+  height: 1200px;
+  width: 1200px;
+
+  top: -40%;
+  left: -25%;
+}
+
+.circle2 {
+  height: 1200px;
+  width: 1200px;
+
+  top: -40%;
+  right: -25%;
+}
+
 .bg {
   position: absolute;
   top: 50px;
