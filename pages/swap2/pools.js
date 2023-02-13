8f9bccefa582c0aa18f50baec90554d9cba437f1
clayzenx <clay.zenx@gmail.com>
2023-02-13 13:01:42 +0700

    Liquidity id page

---
+++ pages/swap2/pools.js
@@ -0,0 +1,139 @@
+// TODO mock pools
+export const pools = [
+  {
+    id: 1,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: true,
+    inputEarning: 552,
+    outputEarning: -21,
+    percent: 1
+  },
+  {
+    id: 2,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  },
+  {
+    id: 3,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  },
+  {
+    id: 4,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  },
+  {
+    id: 5,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  },
+  {
+    id: 6,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  },
+  {
+    id: 7,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  },
+  {
+    id: 8,
+    input: {
+      symbol: 'BRWL',
+      contract: 'brawlertoken'
+    },
+    output: {
+      symbol: 'TLM',
+      contract: 'alien.worlds'
+    },
+    min: 4.52,
+    max: 12.512,
+    inRange: false,
+    inputEarning: -552,
+    outputEarning: 21,
+    percent: 1
+  }
+]
