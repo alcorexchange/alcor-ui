a82367a7c45c83427493d91797696fe0ac708f06
Avral <avral@pm.me>
2023-02-15 01:32:19 +0300

    pin 726

--- config.js
+++ config.js
@@ -346,7 +346,7 @@ const networks = {
     withdraw: {},
 
     RECOMMENDED_MARKETS: ['TLM@tlm-alien.worlds'],
-    PINNED_MARKETS: [156],
+    PINNED_MARKETS: [156, 726],
     BANNER_MARKETS: [156],
 
     SCAM_CONTRACTS: [
