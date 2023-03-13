77ee066238c1e422d4b9a3f2ee1b6e5e58ecf0c3
sa8ab <bwsaeed8@gmail.com>
2023-03-13 08:37:52 +0100

    ^ PageHeader responsive

--- components/amm/PageHeader.vue
+++ components/amm/PageHeader.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-.page-header.d-flex.justify-content-between.align-items-center.pb-2
-  ReturnLink.fs-20
-  span.align-self-center.title {{ title }}
-  slot(name='end')
-    .d-flex.gap-4.align-items-center
-      AlcorButton.action(iconOnly flat)
-        i.el-icon-refresh.pointer.fs-18(@click="$emit('refresh')")
-      Settings.settings
+.page-header.d-flex.align-items-center.pb-2
+  ReturnLink.start.fs-20
+  span.title {{ title }}
+  .end
+    slot(name='end')
+      .d-flex.gap-4.align-items-center
+        AlcorButton.action(iconOnly flat)
+          i.el-icon-refresh.pointer.fs-18(@click="$emit('refresh')")
+        Settings.settings
 </template>
 
 <script>
@@ -27,9 +28,19 @@ export default {
 <style scoped lang="scss">
 .page-header {
   border-bottom: 1px solid var(--border-color);
+  gap: 8px;
+  > * {
+    flex: 1;
+  }
+  .end {
+    display: flex;
+    justify-content: flex-end;
+  }
   .title{
     font-weight: 500;
     font-size: 1.1rem;
+    display: flex;
+    justify-content: center;
   }
 }
 .action::v-deep, .settings::v-deep .action {
@@ -37,5 +48,15 @@ export default {
   height: 32px;
   border-radius: 50%;
   padding: 0;
+}
+@media only screen and (max-width: 380px){
+  .page-header{
+    .start{
+      flex: initial;
+    }
+    .title{
+      justify-content: start;
+    }
+  }
 }
 </style>
