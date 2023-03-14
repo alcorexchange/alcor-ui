8d7d0845396925086c77f8d1859584ede074e55f
sa8ab <bwsaeed8@gmail.com>
2023-03-14 07:07:29 +0100

    ^ AuthOnly button style

--- components/AuthOnly.vue
+++ components/AuthOnly.vue
@@ -3,5 +3,22 @@
   template(v-if="$store.state.user")
     slot
   .d-flex.justify-content-center(v-else)
-    el-button(type="default" @click='$store.dispatch("chain/mainLogin")') {{ $t('Connect Wallet') }}
+    AlcorButton.connect(type="default" @click='$store.dispatch("chain/mainLogin")') {{ $t('Connect Wallet') }}
 </template>
+
+<script>
+import AlcorButton from '@/components/AlcorButton'
+export default {
+  components: {
+    AlcorButton
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.connect{
+  width: 100%;
+  padding: 16px;
+  border-radius: 6px;
+}
+</style>
