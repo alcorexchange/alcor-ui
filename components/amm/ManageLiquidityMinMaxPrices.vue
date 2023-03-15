92f68071e2a31df0eeb5b7728fa59f996935f86d
sa8ab <bwsaeed8@gmail.com>
2023-03-15 23:39:54 +0100

    + composed percent of existing pool

--- components/amm/ManageLiquidityMinMaxPrices.vue
+++ components/amm/ManageLiquidityMinMaxPrices.vue
@@ -4,12 +4,12 @@
     template(#top)
       .fs-12.text-center Min Price
     .fs-12.text-center {{ tokenA.symbol }} per {{ tokenB.symbol }}
-    .fs-12.text-center.disable Your position 100% composed of {{ tokenB.symbol }} at this price
+    .fs-12.text-center.disable Your position {{ getTokenComposedPercent('w') }}% composed of {{ tokenB.symbol }} at this price
   InputStepCounter.flex-1(:readOnly='true', :value='(tokensInverted ? priceUpper.invert() : priceUpper).toSignificant(5)')
     template(#top)
       .fs-12.text-center Max Price
     .fs-12.text-center {{ tokenA.symbol }} per {{ tokenB.symbol }}
-    .fs-12.text-center.disable Your position 100% composed of {{ tokenB.symbol }} at this price
+    .fs-12.text-center.disable Your position {{ getTokenComposedPercent('b') }}% composed of {{ tokenB.symbol }} at this price
 </template>
 
 <script>
@@ -27,6 +27,18 @@ export default {
 
     tokenB() {
       return this.tokensInverted ? this.position.pool.tokenA : this.position.pool.tokenB
+    }
+  },
+  methods: {
+    getTokenComposedPercent(side) {
+      const amountA = parseFloat(this.position.amountA.toFixed())
+      const amountB = parseFloat(this.position.amountB.toFixed())
+      const total = amountA + amountB
+
+      const aPercent = ((100 * amountA) / total).toFixed(0)
+      const bPercent = ((100 * amountB) / total).toFixed(0)
+
+      return side === 'w' ? aPercent : bPercent
     }
   }
 }
