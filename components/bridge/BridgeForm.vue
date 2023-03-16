d479c3eb7de62a05df898572652d007b867eedfd
sa8ab <bwsaeed8@gmail.com>
2023-03-16 10:44:00 +0100

    ^

--- components/bridge/BridgeForm.vue
+++ components/bridge/BridgeForm.vue
@@ -102,7 +102,7 @@
       //alcor-button.transfer-btn(access @click="transfer") Transfer and Prove
       //alcor-button.transfer-btn(outline @click="clear") clear
 
-    BridgeSlider(v-if="true" :steps="steps" :step="step")
+    BridgeSlider(v-if="inProgress" :steps="steps" :step="step")
 
     div(v-if="step === 4")
       .d-flex.justify-content-center.text-center.mt-4
