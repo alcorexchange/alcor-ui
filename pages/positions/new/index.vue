6c539c1e665958ba20d55d6da96ae143c7bb3238
Avral <avral@pm.me>
2023-03-30 21:27:20 +0300

    fix submit button disable state

--- pages/positions/new/index.vue
+++ pages/positions/new/index.vue
@@ -32,7 +32,7 @@
         | amountA: {{ amountA }}
         | amountB: {{ amountB }}
         AuthOnly.w-100
-          AlcorButton.submit(@click='submit',:class='{ disabled: sumbitButton.state }' :disabled="sumbitButton.state") {{ sumbitButton.text }}
+          AlcorButton.submit(@click='submit',:class='{ disabled: submitDisabled.state }' :disabled="submitDisabled.state") {{ submitDisabled.text }}
       //- 3 end
 
       //- 4 start end is end of page
@@ -259,17 +259,14 @@ export default {
       ]
     },
 
-    sumbitButton() {
-      const { depositADisabled, depositBDisabled, outOfRange, amountA, amountB } = this
+    submitDisabled() {
+      const { inputADisabled, inputBDisabled, amountA, amountB } = this
 
       if (
-        depositADisabled && depositBDisabled
-      ) return { state: true, text: 'Enter Amount' }
-      if (
-        (outOfRange && depositADisabled && !amountB) ||
-        (outOfRange && depositBDisabled && !amountA) ||
-        (!amountA && !amountB)
-      ) return { state: true, text: 'Enter Amount' }
+        (inputADisabled && amountB) ||
+        (inputBDisabled && amountA) ||
+        (amountA && amountB)
+      ) return { state: false, text: 'Add Liquidity' }
 
       return { state: false, text: 'Add Liquidity' }
     },
@@ -470,10 +467,6 @@ export default {
     }
   },
 
-  mounted() {
-    this.init()
-  },
-
   methods: {
     ...mapActions('modal', ['previewLiquidity']),
 
@@ -531,19 +524,9 @@ export default {
       this.onInputAmountA(this.amountA)
     },
 
-    init() {
-
-      //this.parseUrlParams()
-      //this.checkPosition()
-
-      //this.tokenA = this.tokens[1]
-      //this.tokenB = this.tokens[2]
-    },
-
     onInputAmountA(value) {
       if (!value) return this.amountB = null
       const dependentAmount = this.getDependedAmount(value, 'CURRENCY_A')
-      console.log({ dependentAmount })
       if (dependentAmount) {
         this.amountB = dependentAmount.toFixed()
         console.log(dependentAmount.toFixed(undefined, undefined, Rounding.ROUND_UP), dependentAmount.toFixed())
@@ -892,6 +875,7 @@ export default {
       onRightRangeInput(rightPrice.toFixed(5))
     },
   },
+
   watch: {
     tokenA(token) {
       setTimeout(() => {
