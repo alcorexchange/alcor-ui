1046f98572286da35afbdd5868c95b5f516de38c
Avral <avral@pm.me>
2023-04-27 13:42:36 +0800

    update default hoop

--- server/services/apiV2Service/swapRouter.ts
+++ server/services/apiV2Service/swapRouter.ts
@@ -16,7 +16,7 @@ import { getRedisTicks, getPools } from '../swapV2Service/utils'
 
 export const swapRouter = Router()
 
-const TRADE_OPTIONS = { maxNumResults: 1, maxHops: 4 }
+const TRADE_OPTIONS = { maxNumResults: 1, maxHops: 3 }
 
 swapRouter.get('/getRoute', async (req, res) => {
   const network: Network = req.app.get('network')
