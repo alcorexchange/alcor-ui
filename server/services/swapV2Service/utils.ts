46ce56f39e349b391e4eef0f5c3edb76ab902382
Avral <avral@pm.me>
2023-04-29 21:26:29 +0800

    update top nav

--- server/services/swapV2Service/utils.ts
+++ server/services/swapV2Service/utils.ts
@@ -33,6 +33,7 @@ export async function getPools(chain: string) {
   for (const p of mongoPools) {
     const ticks = await getRedisTicks(chain, p.id)
 
+    console.log('ticks of', p.id, Array.from(ticks.values()).sort((a, b) => a.id - b.id))
     pools.push(new Pool({
       ...p,
       tokenA: new Token(p.tokenA.contract, p.tokenA.decimals, p.tokenA.symbol, p.tokenA.id),
