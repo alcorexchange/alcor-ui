29784fdb9727a8c9d5d8d35f6004811967e1993b
Avral <avral@pm.me>
2023-05-04 23:31:57 +0800

    add positions get by id

--- server/services/apiV2Service/swap.ts
+++ server/services/apiV2Service/swap.ts
@@ -127,6 +127,23 @@ swap.get('/pools/:id', async (req, res) => {
   res.json(pool)
 })
 
+swap.get('/pools/positions/:id', async (req, res) => {
+  const network: Network = req.app.get('network')
+  const redis = req.app.get('redisClient')
+
+  const pool = await getPoolInstance(network.name, req.params.id)
+  const positions = JSON.parse(await redis.get(`positions_${network.name}`))
+
+  const p = positions.find(p => p.pool == req.params.id)
+
+  const position = new Position({ ...p, pool })
+
+  p.amountA = position.amountA.toAsset()
+  p.amountB = position.amountB.toAsset()
+
+  res.json(p)
+})
+
 swap.get('/pools/:id/positions', async (req, res) => {
   const network: Network = req.app.get('network')
   const redis = req.app.get('redisClient')
