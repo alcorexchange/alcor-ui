71f5336a477414b45f65a3d1c81cd89fc7a7571f
sa8ab <bwsaeed8@gmail.com>
2023-05-09 19:02:49 +0430

    add actions to tokenheader

--- components/analytics/AnalyticsTokenHeader.vue
+++ components/analytics/AnalyticsTokenHeader.vue
@@ -1,11 +1,21 @@
 <template lang="pug">
-.analytics-token-header
-  .name-and-image
-    img.image(:src="$tokenLogo('USDT', 'usdt.alcor')" )
-    .name.fs-32 USDT
-  .contract.fs-20 (usdt.alcor)
-  .price.fs-24 $12,010
-  .change.fs-14 +8.40%
+.header-container
+  .analytics-token-header.mb-4
+    .name-and-image
+      img.image(:src="$tokenLogo('USDT', 'usdt.alcor')" )
+      .name.fs-32 USDT
+    .contract.fs-20 (usdt.alcor)
+    .price.fs-24 $12,010
+    .change.fs-14 +8.40%
+  .actions
+    AlcorButton Website
+    el-select(placeholder="Explorers")
+     el-option(:value="0")
+    el-select(placeholder="Networks")
+     el-option(:value="0")
+    el-select(placeholder="Socials")
+     el-option(:value="0")
+    AlcorButton Source Code
 </template>
 
 <script>
@@ -37,6 +47,23 @@ export default {
   }
   .name, .price {
     line-height: 1.2;
+  }
+}
+.actions {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  ::v-deep {
+    .el-input__inner {
+      width: 120px;
+      padding-left: 8px;
+      height: 32px;
+      line-height: 32px;
+    }
+    .el-input__suffix {
+      display: flex;
+      align-items: center;
+    }
   }
 }
 </style>
