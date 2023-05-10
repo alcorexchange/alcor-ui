7386fdc26cdbf540eac4f2f8d2e9e8520ad68b65
sa8ab <bwsaeed8@gmail.com>
2023-05-10 17:59:47 +0430

    + analytics pool page

---
+++ components/analytics/AnalyticsPairHeader.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+.header-container
+  .analytics-pair-header
+    .name-and-image
+      PairIcons.pair-icons(
+        size="24"
+        :token1="{contract: 'eosio.token', symbol: 'WAX'}"
+        :token2="{contract: 'usdt.alcor', symbol: 'USDT'}"
+      )
+      .name.fs-32 WAX/USDT
+    span.tag.fs-14 0.3%
+</template>
+
+<script>
+import AlcorButton from '@/components/AlcorButton'
+import PairIcons from '@/components/PairIcons'
+export default {
+  name: 'AnalyticsPairHeader',
+  components: {
+    AlcorButton,
+    PairIcons
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.analytics-pair-header {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  padding: 12px 0;
+  .pair-icons {
+    width: 32px !important;
+    height: 32px !important;
+  }
+  .name-and-image {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+  // .name {
+  //   line-height: 1.2;
+  // }
+  .tag {
+    background: var(--background-color-third);
+    padding: 4px 4px;
+    border-radius: 4px;
+    display: flex;
+    line-height: 1rem;
+  }
+}
+</style>
--- pages/analytics/_id/_pair.vue
+++ pages/analytics/_id/_pair.vue
@@ -1,3 +1,34 @@
 <template lang="pug">
-  .pair-analytics-page
+.pair-analytics-page
+  AnalyticsHeader
+  AnalyticsBreadcrumb(
+    :token="{symbol: 'WAX', contract: 'wax.alcor'}"
+    pair="WAX/BLK"
+  )
+  AnalyticsPairHeader
+  .analytics-stats-and-chart
+    AnalyticsStats
+    AnalyticsChart
 </template>
+
+<script>
+import AnalyticsHeader from '@/components/analytics/AnalyticsHeader'
+import AnalyticsStats from '@/components/analytics/AnalyticsStats'
+import AnalyticsChart from '@/components/analytics/AnalyticsChart'
+import AnalyticsSectionHeader from '@/components/analytics/AnalyticsSectionHeader'
+import AnalyticsBreadcrumb from '@/components/analytics/AnalyticsBreadcrumb'
+import AnalyticsPairHeader from '@/components/analytics/AnalyticsPairHeader'
+import AlcorButton from '@/components/AlcorButton'
+
+export default {
+  components: {
+    AnalyticsHeader,
+    AnalyticsStats,
+    AnalyticsChart,
+    AnalyticsSectionHeader,
+    AlcorButton,
+    AnalyticsBreadcrumb,
+    AnalyticsPairHeader
+  }
+}
+</script>
