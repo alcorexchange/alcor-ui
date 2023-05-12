e1ee752ed835f5108aa56a17e4e437034c7e8963
sa8ab <bwsaeed8@gmail.com>
2023-05-13 01:13:36 +0330

    ^ graymass image

--- components/landing/SupportedChains.vue
+++ components/landing/SupportedChains.vue
@@ -47,8 +47,8 @@
 
       span.title {{ $t('PARTNERS_PROVIDERS') }}
       .items
-        a.item(v-for="{ image, url, padding } in items" :href="url" target="_blank" :style="{ padding }")
-          img(:src="image")
+        a.item(v-for="{ image, lightImage, url, padding } in items" :href="url" target="_blank" :style="{ padding }")
+          img(:src="$colorMode.value == 'dark' ? image : lightImage")
         a(href="https://eosamsterdam.net/")
           EOS(v-if="$colorMode.value == 'dark'")
           img(v-else src='~/assets/images/partners-1.svg')
@@ -69,7 +69,8 @@ export default {
   data: () => ({
     items: [
       {
-        image: 'https://bloks.io/img/history/greymass.svg',
+        image: require('@/assets/icons/Graymass_white.svg'),
+        lightImage: require('@/assets/icons/Graymass.svg'),
         url: 'https://greymass.com/en/'
       },
     ]
