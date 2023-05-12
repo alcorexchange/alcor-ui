3efd8521827b0165c2f68a3610e2f2bbbf43c960
Avral <avral@pm.me>
2023-05-12 22:34:02 +0800

    add debug

--- server/services/apiV2Service/tokens.ts
+++ server/services/apiV2Service/tokens.ts
@@ -3,18 +3,19 @@ import { getAllTokensWithPrices } from '../updaterService/prices'
 
 export const tokens = Router()
 
+tokens.get('/tokens/:id', async (req, res) => {
+  const network: Network = req.app.get('network')
+
+  console.log('try get tokens', req.params.id)
+  const tokens = await getAllTokensWithPrices(network)
+
+  res.json(tokens.find(t => t.id == req.params.id))
+})
+
 tokens.get('/tokens', async (req, res) => {
   const network: Network = req.app.get('network')
 
   const tokens = await getAllTokensWithPrices(network)
 
   res.json(tokens)
-})
-
-tokens.get('/tokens/:id', async (req, res) => {
-  const network: Network = req.app.get('network')
-
-  const tokens = await getAllTokensWithPrices(network)
-
-  res.json(tokens.find(t => t.id == req.params.id))
 })
