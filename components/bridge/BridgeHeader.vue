9343937319b716a3b152ae68a359e7d3d040de2b
sa8ab <bwsaeed8@gmail.com>
2023-05-27 11:07:26 +0330

    bridge history index exact

--- components/bridge/BridgeHeader.vue
+++ components/bridge/BridgeHeader.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .d-flex.align-items-center.gap-16.p-1
-  NuxtLink.item(:to="localeRoute('/bridge_new')").fs-18 Bridge
+  NuxtLink.item(:to="localeRoute('/bridge_new')" exact).fs-18 Bridge
   NuxtLink.item(:to="localeRoute('/bridge_new/history')").fs-18 History
 </template>
 
