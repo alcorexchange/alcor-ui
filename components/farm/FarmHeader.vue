ad1625de6d4a89acd5fa19b780957cd21250decc
sa8ab <bwsaeed8@gmail.com>
2023-07-28 22:35:28 +0330

    farm init

---
+++ components/farm/FarmHeader.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .farm-header
+    .left
+      el-input(placeholder="Search Tokens")
+      AlcorSwitch(
+        one="Active"
+        two="Finished"
+      )
+      .stacked-only
+        el-switch(
+          active-text="Stacked only"
+        )
+    .right
+</template>
+
+<script>
+import AlcorSwitch from '@/components/AlcorSwitch'
+export default {
+  name: 'FarmHeader',
+  components: {
+    AlcorSwitch,
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/farms/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .farms-page FARMS
+</template>
+
+<script>
+import FarmHeader from '@/components/farm/FarmHeader'
+export default {
+  name: 'FarmsPage',
+  components: {
+    FarmHeader,
+  },
+}
+</script>
+
+<style scoped lang="scss"></style>
