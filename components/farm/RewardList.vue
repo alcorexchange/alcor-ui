454360a17a260aeb9d9064a90aaf5f1fba0b4d32
sa8ab <bwsaeed8@gmail.com>
2023-08-02 12:44:04 +0330

    add reward list

---
+++ components/farm/RewardList.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+.rewards-list
+  .header
+    .farm-create-section-title Select Reward Token
+    Note You can only select whitelisted tokens
+  //- inputs
+  .inputs
+    slot
+  AlcorButton(class="add-button" @click="$emit('newReward')" bordered)
+    i.el-icon-plus
+    span Add Another Reward
+</template>
+
+<script>
+import AlcorButton from '@/components/AlcorButton'
+import Note from '@/components/farm/Note'
+export default {
+  name: 'RewardsList',
+  components: {
+    AlcorButton,
+    Note,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.inputs {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin: 8px 0;
+}
+.add-button::v-deep {
+  padding: 10px 14px;
+  border-color: var(--main-green) !important;
+  color: var(--main-green);
+
+  &:hover {
+    background: var(--hover) !important;
+  }
+}
+</style>
--- pages/farms/create.vue
+++ pages/farms/create.vue
@@ -3,11 +3,16 @@
   AlcorContainer(class="alcor-container")
     PageHeader(class="page-header" title="Create Farm")
       template(#end) &nbsp;
+
     Note(class="mt-2 fs-14") You can only create a farm for a base token that you are the project owner of.
 
     TokenSelection(class="mt-3")
 
     FeeTierSelection(:options="feeOptions" class="mt-3"  :value="1")
+
+    RewardList(class="mt-3" @newReward="onNewReward")
+      FarmTokenInput(v-for="reward, index in rewardList" :canRemove="index > 0" @remove="onRemoveReward(index)")
+
 </template>
 
 <script>
@@ -16,6 +21,8 @@ import PageHeader from '@/components/amm/PageHeader'
 import Note from '@/components/farm/Note'
 import TokenSelection from '@/components/farm/TokenSelection'
 import FeeTierSelection from '@/components/farm/FeeTierSelection'
+import RewardList from '@/components/farm/RewardList'
+import FarmTokenInput from '@/components/farm/FarmTokenInput'
 export default {
   name: 'CreateFarm',
   components: {
@@ -24,11 +31,26 @@ export default {
     Note,
     TokenSelection,
     FeeTierSelection,
+    RewardList,
+    FarmTokenInput,
   },
 
   data: () => ({
     feeOptions: [{ value: 0.05 }, { value: 0.3 }, { value: 1 }],
+    rewardList: [{ token: '', amount: 0 }],
   }),
+
+  methods: {
+    onNewReward() {
+      this.rewardList.push({
+        token: '',
+        amount: 0,
+      })
+    },
+    onRemoveReward(index) {
+      this.rewardList = this.rewardList.filter((_, i) => i !== index)
+    },
+  },
 }
 </script>
 
