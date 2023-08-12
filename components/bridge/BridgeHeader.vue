c6f059585e0b3a7e40adf4bb75908de8968ade3c
sa8ab <bwsaeed8@gmail.com>
2023-08-12 13:44:15 +0330

    add history count on bridge

--- components/bridge/BridgeHeader.vue
+++ components/bridge/BridgeHeader.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
 .d-flex.align-items-center.gap-16.p-1
   NuxtLink.item(:to="localeRoute('/bridge_new')" exact).fs-18 Bridge
-  NuxtLink.item(:to="localeRoute('/bridge_new/history')").fs-18 History
+  .history-item-container
+    NuxtLink.item(:to="localeRoute('/bridge_new/history')").fs-18 History
+    .pending-count 10
 </template>
 
 <script>
 export default {
-  name: 'BridgeHeader'
+  name: 'BridgeHeader',
 }
 </script>
 
@@ -18,6 +20,19 @@ export default {
   }
 }
 .active {
-  color: var(--text-default)
+  color: var(--text-default);
+}
+
+.history-item-container {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  .pending-count {
+    padding: 2px 6px;
+    line-height: 1.2;
+    background: #ff2d55;
+    border-radius: 24px;
+    font-size: 0.8rem;
+  }
 }
 </style>
