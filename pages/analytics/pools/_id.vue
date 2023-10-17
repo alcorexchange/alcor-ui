43f9c3d5f5b5f4dfe1c9c6c4fc1ff541072b1338
sa8ab <bwsaeed8@gmail.com>
2023-10-18 00:04:47 +0330

    comment prev table

--- pages/analytics/pools/_id.vue
+++ pages/analytics/pools/_id.vue
@@ -12,78 +12,78 @@ div
     template(#row="{ item }")
       AnalyticsPositionRow.analytics-position-row(:position="item")
 
-  el-table.position-table.custom-responsive-table(
-    v-loading="loading"
-    :data='positions',
-    style='width: 100%',
-    @row-click="showPosition"
-  )
-    template(#empty)
-      .d-flex.flex-column.align-items-center.gap-30.py-5
-        i.el-icon-moon-night.fs-40
-        .fs-14.lh-14 No liquidity positions yet
-    el-table-column(:label='$t("Assets in Position")' width="140" className="assets")
-      template(slot-scope='{row}')
-        AlcorButton(@click="openInNewTab(monitorAccount(row.owner))")
-          span {{ row.owner }}
+  //- el-table.position-table.custom-responsive-table(
+  //-   v-loading="loading"
+  //-   :data='positions',
+  //-   style='width: 100%',
+  //-   @row-click="showPosition"
+  //- )
+  //-   template(#empty)
+  //-     .d-flex.flex-column.align-items-center.gap-30.py-5
+  //-       i.el-icon-moon-night.fs-40
+  //-       .fs-14.lh-14 No liquidity positions yet
+  //-   el-table-column(:label='$t("Assets in Position")' width="140" className="assets")
+  //-     template(slot-scope='{row}')
+  //-       AlcorButton(@click="openInNewTab(monitorAccount(row.owner))")
+  //-         span {{ row.owner }}
 
-    el-table-column(:label='$t("Range")' width="220" class-name="min-max")
-      template(slot-scope='{row}')
-        .d-flex.flex-column
-          .d-flex.align-items-center.gap-4
-            .indicator(:class="{ 'in-range': row.inRange }")
-            .fs-10 {{ row.inRange ? 'In Range': 'Out of Range' }}
-          .d-flex.align-items-center.gap-6.flex-wrap
-            .d-flex.gap-4
-              .fs-12.disable MIN
-              .fs-12.contrast {{ row.priceLower }}
-            i.el-icon-sort.rot
-            .d-flex.gap-4
-              .fs-12.disable MAX
-              .fs-12.contrast {{ row.priceUpper }}
+  //-   el-table-column(:label='$t("Range")' width="220" class-name="min-max")
+  //-     template(slot-scope='{row}')
+  //-       .d-flex.flex-column
+  //-         .d-flex.align-items-center.gap-4
+  //-           .indicator(:class="{ 'in-range': row.inRange }")
+  //-           .fs-10 {{ row.inRange ? 'In Range': 'Out of Range' }}
+  //-         .d-flex.align-items-center.gap-6.flex-wrap
+  //-           .d-flex.gap-4
+  //-             .fs-12.disable MIN
+  //-             .fs-12.contrast {{ row.priceLower }}
+  //-           i.el-icon-sort.rot
+  //-           .d-flex.gap-4
+  //-             .fs-12.disable MAX
+  //-             .fs-12.contrast {{ row.priceUpper }}
 
-    el-table-column(:label='$t("Assets in Pool")' width="180")
-      template(slot-scope='{row}')
-        .d-flex.flex-column
-          .mobile-label {{ $t("Assets in Pool") }}
-          .d-flex.align-items-center.gap-4
-            token-image(:src='$tokenLogo(row.pool.tokenA.symbol, row.pool.tokenA.contract)' height="12")
+  //-   el-table-column(:label='$t("Assets in Pool")' width="180")
+  //-     template(slot-scope='{row}')
+  //-       .d-flex.flex-column
+  //-         .mobile-label {{ $t("Assets in Pool") }}
+  //-         .d-flex.align-items-center.gap-4
+  //-           token-image(:src='$tokenLogo(row.pool.tokenA.symbol, row.pool.tokenA.contract)' height="12")
 
-            .fs-12.d-flex.gap-4
-              span {{ row.amountA }}
-          .d-flex.align-items-center.gap-4
-            token-image(:src='$tokenLogo(row.pool.tokenB.symbol, row.pool.tokenB.contract)' height="12")
+  //-           .fs-12.d-flex.gap-4
+  //-             span {{ row.amountA }}
+  //-         .d-flex.align-items-center.gap-4
+  //-           token-image(:src='$tokenLogo(row.pool.tokenB.symbol, row.pool.tokenB.contract)' height="12")
 
-            .fs-12.d-flex.gap-4(:class="{ red: false }")
-              span {{ row.amountB }}
+  //-           .fs-12.d-flex.gap-4(:class="{ red: false }")
+  //-             span {{ row.amountB }}
 
-    el-table-column(:label='$t("Unclaimed Fees")' width="168" class-name="unclaimed-fees" sortable sort-by='feesA')
-      template(slot-scope='{row}')
-        .mobile-label.unclaimed-fees-label {{ $t("Unclaimed Fees") }}
+  //-   el-table-column(:label='$t("Unclaimed Fees")' width="168" class-name="unclaimed-fees" sortable sort-by='feesA')
+  //-     template(slot-scope='{row}')
+  //-       .mobile-label.unclaimed-fees-label {{ $t("Unclaimed Fees") }}
 
-        .d-flex.flex-column
-          .d-flex.align-items-center.gap-4
-            token-image(:src='$tokenLogo(row.pool.tokenA.symbol, row.pool.tokenA.contract)' height="12")
+  //-       .d-flex.flex-column
+  //-         .d-flex.align-items-center.gap-4
+  //-           token-image(:src='$tokenLogo(row.pool.tokenA.symbol, row.pool.tokenA.contract)' height="12")
 
-            .fs-12.earn.d-flex.gap-4
-              span {{ row.feesA }}
-          .d-flex.align-items-center.gap-4
-            token-image(:src='$tokenLogo(row.pool.tokenB.symbol, row.pool.tokenB.contract)' height="12")
+  //-           .fs-12.earn.d-flex.gap-4
+  //-             span {{ row.feesA }}
+  //-         .d-flex.align-items-center.gap-4
+  //-           token-image(:src='$tokenLogo(row.pool.tokenB.symbol, row.pool.tokenB.contract)' height="12")
 
-            .fs-12.earn.d-flex.gap-4
-              span {{ row.feesB }}
+  //-           .fs-12.earn.d-flex.gap-4
+  //-             span {{ row.feesB }}
 
-    el-table-column(:label='$t("Total Value")' width="100" v-if="!isMobile" sortable sort-by='totalValue')
-      template(slot-scope='{row}')
-        span $ {{ row.totalValue && row.totalValue.toFixed(2) }}
+  //-   el-table-column(:label='$t("Total Value")' width="100" v-if="!isMobile" sortable sort-by='totalValue')
+  //-     template(slot-scope='{row}')
+  //-       span $ {{ row.totalValue && row.totalValue.toFixed(2) }}
 
-    el-table-column(:label='$t("P&L")' width="100" v-if="!isMobile" sortable sort-by='pNl')
-      template(slot-scope='{row}')
-        span(:style="{color: $percentColor(row.pNl)}") $ {{ row.pNl && row.pNl.toFixed(2) }}
+  //-   el-table-column(:label='$t("P&L")' width="100" v-if="!isMobile" sortable sort-by='pNl')
+  //-     template(slot-scope='{row}')
+  //-       span(:style="{color: $percentColor(row.pNl)}") $ {{ row.pNl && row.pNl.toFixed(2) }}
 
-    el-table-column(:label='$t("Action")' v-if="!isMobile" align="right")
-      template(slot-scope='{row}')
-        alcor-button(compact) {{ $t('Manage') }}
+  //-   el-table-column(:label='$t("Action")' v-if="!isMobile" align="right")
+  //-     template(slot-scope='{row}')
+  //-       alcor-button(compact) {{ $t('Manage') }}
 
 </template>
 
