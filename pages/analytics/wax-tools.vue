3e0dc6a5da0cb504281dddf3caed6bfeabcce948
avral <avral@pm.me>
2023-11-26 23:59:30 +0800

    upd

--- pages/analytics/wax-tools.vue
+++ pages/analytics/wax-tools.vue
@@ -2,10 +2,9 @@
 .analytics-page
   AnalyticsHeader
 
-  iframe(loading=lazy src='https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698345895597&to=1700937895597&panelId=30',
-    height='500', frameborder='0')
+  iframe(loading=lazy src='https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698345895597&panelId=30' height='500', frameborder='0')
 
-  iframe(loading=lazy src="https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698346480678&to=1700938480678&panelId=16" height='500', frameborder='0' allowfullscreen)
+  iframe(loading=lazy src="https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698346480678&panelId=16" height='500', frameborder='0')
 </template>
 
 <script>
