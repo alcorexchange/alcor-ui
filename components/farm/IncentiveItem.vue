7dd238dfea74f374997835d3f4e5774139cf9ef8
sa8ab <bwsaeed8@gmail.com>
2023-11-28 02:52:12 +0330

    update advanced table

--- components/farm/IncentiveItem.vue
+++ components/farm/IncentiveItem.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .incentive-item
-  .incentive-header
+  //.incentive-header
     .left
       .key-value
         span.key.muted.fs-14 Daily Reward
@@ -11,25 +11,23 @@
       .key-value
         //- span.muted Remaining Time
         span.fs-14 {{ incentive.daysRemain }} Days
-    .right(v-if="incentive.incentiveStats.length > 0")
-      AlcorButton(access compact @click="$emit('claimAll', incentive)" v-if="!finished").farm-claim-button Claim All
-      AlcorButton(access bordered compact @click="$emit('stakeAll', incentive)" v-if="!finished").farm-stake-button Stake All
-      AlcorButton(:class="finished ? 'access' : 'danger bordered'" compact @click="$emit('unstakeAll', incentive)").farm-unstake-button {{ finished ? 'Claim & Unstake All' : 'Unstake All' }}
   .incentive-content
     table.fs-14
       thead
         tr
-          th Position ID
-          th Pool Share
+          th Your Stake
           th Estimated Daily Reward
           th Farmed Reward
+          th Pool Position
           th
+            .right(v-if="incentive.incentiveStats.length > 0")
+              AlcorButton(access compact @click="$emit('claimAll', incentive)" v-if="!finished").farm-claim-button Claim All
+              AlcorButton(access bordered compact @click="$emit('stakeAll', incentive)" v-if="!finished").farm-stake-button Stake All
+              AlcorButton(:class="finished ? 'access' : 'danger bordered'" compact @click="$emit('unstakeAll', incentive)").farm-unstake-button {{ finished ? 'Claim & Unstake All' : 'Unstake All' }}
       tbody
         template(v-for="stat in incentive.incentiveStats")
           tr(v-if="stat.staked")
             td
-              NuxtLink(:to="localeRoute(`/positions/${stat.posId}`)") # {{ stat.posId }}
-            td {{ stat.userSharePercent }}%
             td
               .icon-and-value
                 TokenImage(:src="$tokenLogo(incentive.reward.quantity.split(' ')[1], incentive.reward.contract)" width="14px" height="14px")
@@ -39,13 +37,17 @@
                 TokenImage(:src="$tokenLogo(incentive.reward.quantity.split(' ')[1], incentive.reward.contract)" width="14px" height="14px")
                 span {{ stat.farmedReward | commaFloat }}
             td
+              .d-flex.flex-column.gap-2
+                NuxtLink.position-link(:to="localeRoute(`/positions/${stat.posId}`)") # {{ stat.posId }}
+                span Pool Share {{ stat.userSharePercent }}%
+            td
               slot(name="actions" :stat="stat")
           tr(v-else)
-            td
-              NuxtLink(:to="localeRoute(`/positions/${stat.posId}`)") # {{ stat.posId }}
             td Not Staked
             td
             td
+            td
+              NuxtLink.position-link(:to="localeRoute(`/positions/${stat.posId}`)") # {{ stat.posId }}
             td
               slot(name="actions" :stat="stat")
 </template>
@@ -66,22 +68,11 @@ export default {
 .incentive-item {
   padding: var(--amm-space-3);
 }
-
-.incentive-header {
+.right {
   display: flex;
-  justify-content: space-between;
-  gap: 8px;
-  padding-bottom: var(--amm-space-3);
-  .left {
-    display: flex;
-    align-items: center;
-    gap: 8px;
-  }
-  .right {
-    display: flex;
-    align-items: center;
-    gap: 4px;
-  }
+  align-items: center;
+  justify-content: flex-end;
+  gap: 4px;
 }
 
 .incentive-content {
@@ -91,8 +82,15 @@ export default {
     min-width: 600px;
     th,
     td {
-      padding: 2px 6px;
+      padding: 8px;
       border-bottom: none;
+
+      &:first-child {
+        padding-left: 0;
+      }
+      &:last-child {
+        padding-right: 0;
+      }
     }
     tr,
     td {
@@ -109,19 +107,8 @@ export default {
   }
 }
 
-.key-value {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-}
-
-.icon-and-value {
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  span {
-    font-size: 0.8rem;
-  }
+.position-link {
+  color: var(--main-green);
 }
 
 .farm-stake-button::v-deep {
@@ -146,12 +133,9 @@ export default {
 }
 
 @media only screen and (max-width: 800px) {
-  .incentive-header {
-    flex-direction: column;
-    .right {
-      flex-wrap: wrap;
-      align-self: flex-end;
-    }
+  .right {
+    flex-wrap: wrap;
+    align-self: flex-end;
   }
 }
 @media only screen and (max-width: 600px) {
