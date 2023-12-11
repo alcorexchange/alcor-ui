5285bcf95ad122ce2d8a3fd5255cc4c842ee9774
avral <avral@pm.me>
2023-12-11 19:29:04 +0800

    update wax tools links & charts

--- pages/analytics/wax-tools.vue
+++ pages/analytics/wax-tools.vue
@@ -2,7 +2,21 @@
 .analytics-page
   AnalyticsHeader
 
-  iframe(loading=lazy src='https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698345895597&panelId=30' height='500', frameborder='0')
+  .flex
+    div
+      | Provided by
+      a(href="https://grafana.waxtools.net/" target="_blank")  WAX Tools
+
+  .flex
+    iframe(loading=lazy
+      src='https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&panelId=47"' height='500', frameborder='0' width="50%")
+
+    iframe(loading=lazy
+      src='https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&panelId=49' height='500', frameborder='0' width="50%")
+
+  iframe(loading=lazy
+    src='https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698345895597&panelId=30'
+    height='500', frameborder='0')
 
   iframe(loading=lazy src="https://grafana.waxtools.net/d-solo/ae1f479a-9dd0-40f2-b35b-59b7fb91deac/swap-stats?orgId=2&refresh=5m&from=1698346480678&panelId=16" height='500', frameborder='0')
 </template>
@@ -11,6 +25,8 @@
 import AnalyticsHeader from '@/components/analytics/AnalyticsHeader'
 
 export default {
+  //layout: 'landing', TODO Full schreen
+
   components: { AnalyticsHeader }
 }
 </script>
