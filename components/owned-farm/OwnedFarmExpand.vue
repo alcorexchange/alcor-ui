87c3b4a873a3af672c6537273d7ecd21201d78e2
sa8ab <bwsaeed8@gmail.com>
2024-01-19 06:35:15 +0330

    add color to status

--- components/owned-farm/OwnedFarmExpand.vue
+++ components/owned-farm/OwnedFarmExpand.vue
@@ -21,7 +21,7 @@
               TokenImage(:src="$tokenLogo(incentive.reward.quantity.split(' ')[1], incentive.reward.contract)" width="14px" height="14px")
               span {{ incentive.reward.quantity.split(' ')[0] | nFormat(incentive.reward.symbol.precision) }}
               span.color-grey-thirdly {{ incentive.reward.quantity.split(' ')[1] }}
-          td {{ incentive.isFinished ? 'Finished' : 'Active' }}
+          td(:class="incentive.isFinished ? 'red': 'green'") {{ incentive.isFinished ? 'Finished' : 'Active' }}
           td {{ incentive.lastUpdateTime | moment('YYYY-MM-DD HH:mm') }}
           td {{ incentive.numberOfStakes }}
           td {{ incentive.durationInDays }}
