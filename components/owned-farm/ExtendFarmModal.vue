3a14913afe7f114ca019e1307342d2793f54bdce
avral <avral@pm.me>
2024-03-24 10:04:36 +0400

    update extend farm modal

--- components/owned-farm/ExtendFarmModal.vue
+++ components/owned-farm/ExtendFarmModal.vue
@@ -1,56 +1,99 @@
 <template lang="pug">
 div
-  //AlcorButton(@click="extend" access) Top up
   AlcorButton(@click="visible = true" access) Top up
 
-  el-dialog(title='zzz' :visible.sync='visible' append-to-body width="400px")
-    h1 ololo
+  el-dialog(title='Extend Farm' :visible.sync='visible' append-to-body width="450px")
+    Note(class="fs-14") The farm duration will be reset to the one originally assigned upon creation. ({{incentive.durationInDays}} Days)
 
+    //.mt-3.mb-2.w-100
+      .fs-14 Select {{ incentive.reward.symbol.symbol }} amount
+
+    FarmTokenInput(
+      label="New Reward Amount"
+      :canRemove="false"
+      :locked="true"
+      :token="token"
+      v-model="amount"
+    ).mt-3
+
+    AlcorButton.mt-3.w-100(class="submit" access @click="submit") Extend Farm
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+import FarmTokenInput from '@/components/farm/FarmTokenInput'
 import AlcorButton from '~/components/AlcorButton'
+import Note from '~/components/farm/Note'
+import SelectToken from '~/components/modals/amm/SelectToken2'
 
 export default {
   components: {
-    AlcorButton
+    AlcorButton,
+    SelectToken,
+    Note,
+    FarmTokenInput
   },
+
+  props: ['incentive'],
 
   data() {
     return {
-      visible: false
+      visible: false,
+      amount: 0
+    }
+  },
+
+  computed: {
+    ...mapState(['network']),
+    ...mapGetters(['user']),
+
+    token() {
+      const reward = this.incentive.reward
+
+      return {
+        contract: reward.contract,
+        symbol: reward.symbol.symbol,
+        currency: reward.symbol.symbol,
+        decimals: reward.symbol.precision
+      }
     }
   },
 
   methods: {
-    extend(incentive) {
-      this.$prompt(`The farm duration will be reset to the one originally assigned upon creation.
-        (${incentive.durationInDays} Days)`, `Amount of
-        ${incentive.reward.quantity.split(' ')[1]} for reward.`, 'Extend Farm', {
-        confirmButtonText: 'OK',
-        cancelButtonText: 'Cancel',
-        inputPattern: /^\d*\.?\d*$/,
-        inputErrorMessage: 'Invalid reward amount',
-      }).then(async ({ value }) => {
-        const actions = [
-          {
-            account: incentive.reward.contract,
-            name: 'transfer',
-            authorization: [this.user.authorization],
+    async submit() {
+      try {
+        await this.extend()
+        this.visible = false
+        console.log('visible', this.visible)
+      } catch (e) {
+        this.$notify({
+          type: 'error',
+          title: 'Extend Farm',
+          message: e.message,
+        })
+      }
+    },
 
-            data: {
-              from: this.user.name,
-              to: this.network.amm.contract,
-              quantity:
-                parseFloat(value).toFixed(incentive.reward.symbol.precision) + ' ' + incentive.reward.symbol.symbol,
-              memo: 'incentreward#' + incentive.id,
-            },
+    async extend() {
+      const { incentive } = this
+
+      const actions = [
+        {
+          account: incentive.reward.contract,
+          name: 'transfer',
+          authorization: [this.user.authorization],
+
+          data: {
+            from: this.user.name,
+            to: this.network.amm.contract,
+            quantity: parseFloat(this.amount).toFixed(incentive.reward.symbol.precision) + ' ' + incentive.reward.symbol.symbol,
+            memo: 'incentreward#' + incentive.id,
           },
-        ]
+        },
+      ]
 
-        await this.$store.dispatch('chain/sendTransaction', actions)
-        await this.$store.dispatch('farms/loadIncentives')
-      })
+      await this.$store.dispatch('chain/sendTransaction', actions)
+      setTimeout(() => this.$store.dispatch('farms/loadIncentives'), 1000)
     },
   },
 }
