5e5b0a3de6297671788d97e92bfdf16f5f11ae70
sa8ab <bwsaeed8@gmail.com>
2024-04-19 00:20:47 +0330

    add tooltip formatter for TVL on swap

--- components/amm/SwapChart.vue
+++ components/amm/SwapChart.vue
@@ -48,7 +48,7 @@ alcor-container.p-3.w-100.chart-container-inner
     :color="activeTab === 'Tvl' ? '#723de4' : undefined"
     style="min-height: 400px"
     :events="chartEvents"
-    :tooltipFormatter="activeTab === 'Tvl' ? (v) => `$${v}`: undefined"
+    :tooltipFormatter="renderTooltipFormatter"
   )
 </template>
 
@@ -93,6 +93,9 @@ export default {
   }),
 
   computed: {
+    renderTooltipFormatter() {
+      return this.activeTab === 'TVL' ? (v) => `$${this.$options.filters.commaFloat(v, 2)}` : undefined
+    },
     tabs() {
       return [
         { label: this.$t('Price'), value: 'Price' },
@@ -102,13 +105,7 @@ export default {
         // { label: 'Depth', value: 'Depth' }
       ]
     },
-    ...mapGetters('amm/swap', [
-      'tokenA',
-      'tokenB',
-      'isSorted',
-      'sortedA',
-      'sortedB',
-    ]),
+    ...mapGetters('amm/swap', ['tokenA', 'tokenB', 'isSorted', 'sortedA', 'sortedB']),
     ...mapState('amm', ['pools']),
 
     series() {
@@ -144,12 +141,7 @@ export default {
 
       if (sortedA && sortedB) {
         data = this.charts.map((c) => {
-          const price = new Price(
-            sortedA,
-            sortedB,
-            Q128,
-            JSBI.multiply(JSBI.BigInt(c.price), JSBI.BigInt(c.price))
-          )
+          const price = new Price(sortedA, sortedB, Q128, JSBI.multiply(JSBI.BigInt(c.price), JSBI.BigInt(c.price)))
 
           return {
             x: c._id,
