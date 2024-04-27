525da1909ece0d48c781f9e86c95753936febd0f
sa8ab <bwsaeed8@gmail.com>
2024-04-27 21:02:50 +0330

    update unstake list

--- components/staking/PendingUnstake.vue
+++ components/staking/PendingUnstake.vue
@@ -1,14 +1,85 @@
 <template lang="pug">
-  .pending-unstake Pending unstakes will show here
-    .items
-      .item(v-for="item in unstakes") {{ item.id }}
+  .pending-unstake
+    .fs-18.pt-3.pb-2.disable My Pending Unstakes
+    template(v-if="unstakes?.length")
+      ElAlert.mb-4(:closable="false")
+        template(#title) The requests can be proccessed within one day. It will automatically be sent to [Account Name], you don't need to claim them.
+      .items.px-1
+        .item(v-for="item in unstakes")
+          .header.pb-1.fs-12.disable
+            .date {{ item.request_time | moment('MM-DD HH:mm') }}
+            .id # {{ item.id }}
+          .content
+            .section
+              .section-type.disable.pb-1.fs-14 Pending
+              .section-amount
+                TokenImage(:src="$tokenLogo(item.withdrawToken.quantity.split(' ')[1], item.withdrawToken.contract)" height="20")
+                .amount {{ item.withdrawToken.quantity.split(' ')[0] }}
+                .symbol.fs-14.disable {{ item.withdrawToken.quantity.split(' ')[1] }}
+            .section
+              .section-type.disable.pb-1.fs-14 Bruned
+              .section-amount
+                TokenImage(:src="$tokenLogo(item.requestToken.quantity.split(' ')[1], item.requestToken.contract)" height="20")
+                .amount {{ item.requestToken.quantity.split(' ')[0] }}
+                .symbol.fs-14.disable {{ item.requestToken.quantity.split(' ')[1] }}
+    .no-items.disable(v-else)
+      i.el-icon-moon-night
+      div.fs-14 Your pending unstakes will be shown here.
 </template>
 
 <script>
+import TokenImage from '@/components/elements/TokenImage'
+
 export default {
   name: 'PendingUnstake',
+  components: {
+    TokenImage,
+  },
   props: ['unstakes'],
 }
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.items {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-height: 400px;
+  overflow: auto;
+  .item {
+    display: flex;
+    flex-direction: column;
+    .header {
+      display: flex;
+      justify-content: space-between;
+    }
+    .content {
+      display: flex;
+      justify-content: space-between;
+      gap: 10px;
+    }
+    .section {
+      display: flex;
+      flex-direction: column;
+      .section-title {
+        padding-bottom: 4px;
+      }
+      .section-amount {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+      }
+    }
+  }
+}
+.no-items {
+  padding: 28px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  i {
+    font-size: 2rem;
+  }
+}
+</style>
