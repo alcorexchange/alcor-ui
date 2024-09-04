5f249618b79b28fae6f0fb9fead946a182e7aa75
sa8ab <bwsaeed8@gmail.com>
2024-09-04 12:46:53 +0330

    add searching accounts

--- components/layout/LayoutHeaderSearch.vue
+++ components/layout/LayoutHeaderSearch.vue
@@ -57,7 +57,7 @@
                   span.muted.fs-10 {{ item.tokenA.contract }} / {{ item.tokenB.contract }}
           .no-items(v-else) No Pools Found.
 
-        LayoutSearchAccounts(v-else key="accounts")
+        Accounts(v-else key="accounts" :search="search")
 </template>
 
 <script>
--- components/layout/LayoutSearchAccounts.vue
+++ components/layout/LayoutSearchAccounts.vue
@@ -1,11 +1,66 @@
 <template lang="pug">
-  .accounts
+  .layout-search-accounts
+    .empty(v-if="!search")
+      span Search for accounts
+    .accounts-list(v-else-if="accounts.length")
+      .item(v-for="account in accounts")
+        div {{ account.scope }}
+    .empty(v-else-if="!loading")
+      span No accounts found!
+    .empty-loading.empty(v-else)
+      span loading
 </template>
 
 <script>
 export default {
   name: 'LayoutSearchAccounts',
+  props: ['search'],
+  data: () => ({
+    accounts: [],
+
+    // To prevent showing "No Accounts found" when loading
+    loading: false,
+  }),
+
+  watch: {
+    search: {
+      handler(s) {
+        if (!s) {
+          this.accounts = []
+          return
+        }
+        this.getAccounts(s)
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    async getAccounts(search) {
+      this.loading = true
+      try {
+        const { rows } = await this.$rpc.get_table_by_scope({
+          code: 'eosio',
+          limit: 10,
+          lower_bound: search,
+          upper_bound: search + 'zzzz',
+          table: 'userres',
+        })
+
+        this.accounts = rows
+      } catch (e) {
+        this.accounts = []
+      } finally {
+        this.loading = false
+      }
+    },
+  },
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.empty {
+  padding: 40px 8px;
+  text-align: center;
+}
+</style>
