c6959bdc595bb7fde6d8bf103616f6e4d4db902f
sa8ab <bwsaeed8@gmail.com>
2024-09-06 12:15:58 +0330

    init modal

---
+++ components/modals/AnnouncementModal.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  ElDialog(:visible.sync="visible")
+    .content Some content here ...
+</template>
+
+<script>
+export default {
+  name: 'AnnouncementModal',
+  data: () => ({
+    id: 'wax-on-usd-notice',
+    visible: false,
+  }),
+
+  mounted() {
+    this.tryShowModal()
+  },
+
+  methods: {
+    tryShowModal() {
+      this.visible = true
+    },
+  },
+}
+</script>
+
+<style></style>
