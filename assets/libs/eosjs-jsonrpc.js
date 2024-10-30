0a93899878c77819044df06278b2746fc8ec7818
avral <avral@pm.me>
2024-10-30 17:43:51 +0800

    fix

--- assets/libs/eosjs-jsonrpc.js
+++ assets/libs/eosjs-jsonrpc.js
@@ -300,7 +300,7 @@ export class JsonRpc {
       packed_context_free_data: arrayToHex(
         serializedContextFreeData || new Uint8Array(0)
       ),
-      packed_trx: Array.isArray(serializedTransaction) ? arrayToHex(serializedTransaction) : serializedTransaction
+      packed_trx: typeof serializedTransaction === 'string' ? serializedTransaction : arrayToHex(serializedTransaction)
     })
   }
 
@@ -325,7 +325,7 @@ export class JsonRpc {
         packed_context_free_data: arrayToHex(
           serializedContextFreeData || new Uint8Array(0)
         ),
-      packed_trx: Array.isArray(serializedTransaction) ? arrayToHex(serializedTransaction) : serializedTransaction
+      packed_trx: typeof serializedTransaction === 'string' ? serializedTransaction : arrayToHex(serializedTransaction)
       }
     })
   }
