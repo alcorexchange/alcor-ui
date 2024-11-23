55b07c12baebb69346b7676a7ed02e40706223f8
avral <avral@pm.me>
2024-11-23 15:13:42 +0300

    add index

--- server/models.ts
+++ server/models.ts
@@ -139,6 +139,7 @@ const MatchSchema = new mongoose.Schema({
   time: { type: Date, index: true },
   block_num: { type: Number },
 })
+MatchSchema.index({ chain: 1, market: 1, time: 1 }, { background: true })
 MatchSchema.index({ chain: 1, market: 1, time: -1 }, { background: true })
 MatchSchema.index({ chain: 1, market: 1, asker: 1, bidder: 1, time: -1 }, { background: true })
 MatchSchema.index({ chain: 1, market: 1, time: -1, unit_price: -1 }, { background: true })
@@ -284,6 +285,7 @@ const SwapChartPointSchema = new mongoose.Schema({
   // We not user default here actually
   time: { type: Date, default: () => Date.now(), index: true }
 })
+SwapChartPointSchema.index({ chain: 1, pool: 1, time: 1 }, { background: true })
 SwapChartPointSchema.index({ chain: 1, pool: 1, time: -1 }, { background: true })
 SwapChartPointSchema.index({ chain: 1, pool: 1, time: -1, price: 1, reserveA: 1, reserveB: 1, volumeUSD: 1, usdReserveA: 1, usdReserveB: 1 }, { background: true })
 
@@ -307,6 +309,7 @@ const SwapSchema = new mongoose.Schema({
 SwapSchema.index({ chain: 1, pool: 1, sender: 1, recipient: 1, time: -1 }, { background: true })
 SwapSchema.index({ chain: 1, pool: 1, time: -1 }, { background: true })
 SwapSchema.index({ chain: 1, sender: 1, recipient: 1, time: -1 }, { background: true })
+SwapSchema.index({ chain: 1, time: -1, sender: 1, recipient: 1 }, { background: true })
 SwapSchema.index({ chain: 1, pool: 1, time: -1, tokenA: 1 }, { background: true })
 
 const PositionSchema = new mongoose.Schema({
