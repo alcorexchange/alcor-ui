d6443c1b5ef27c1534ef2dc0b0752a91178055b0
avral <avral@pm.me>
2025-01-11 09:39:11 +0300

    upd

--- config.js
+++ config.js
@@ -227,7 +227,7 @@ const networks = {
       'celestia', 'moneroxmr', 'arbitrum', 'toncoinxpr', 'polkadotx', 'hederax', 'icpx', 'protoninj', 'protonfile', 'xlinkchain',
       'nearx', 'celestia.xpr', 'optimism.xpr', 'ecash.xpr', 'fantomx', 'algorandx', 'casinoxpr', 'galagamesx', 'xcore.xpr', 'xports',
       'xopencoinx', 'protonsara', 'audius', 'bep2', 'sonicx', 'zcashxpr', 'aptos.xpr', 'braveprivacy', 'waxblock',
-      'tronx', 'polkadotx', 'seinetwork'
+      'tronx', 'polkadotx', 'seinetwork', 'deroq'
     ],
     CEX_CONTRACTS: [],
 
