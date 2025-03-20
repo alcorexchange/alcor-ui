14f587fcf4a750a5928fc62cc6f71fa21a7d7b6a
avral <avral@pm.me>
2025-03-21 05:13:17 +0800

    enable updating routes cache on new pool

--- server/services/swapV2Service/index.ts
+++ server/services/swapV2Service/index.ts
@@ -447,22 +447,22 @@ export async function onSwapAction(message: string) {
     await updateTokensPrices(networks[chain]) // Update right away so other handlers will have tokenPrices
 
     // Lead to high load
-    // try {
-    //   const quantityA = data.tokenA.quantity || data.tokenA.asset
-    //   const quantityB = data.tokenB.quantity || data.tokenB.asset
+    try {
+      const quantityA = data.tokenA.quantity || data.tokenA.asset
+      const quantityB = data.tokenB.quantity || data.tokenB.asset
 
-    //   const contractA = data.tokenA.contract || data.tokenA.Contract
-    //   const contractB = data.tokenB.contract || data.tokenB.Contract
+      const contractA = data.tokenA.contract || data.tokenA.Contract
+      const contractB = data.tokenB.contract || data.tokenB.Contract
 
-    //   const tokenA_id = quantityA.split(' ')[1].toLowerCase() + '-' + contractA
-    //   const tokenB_id = quantityB.split(' ')[1].toLowerCase() + '-' + contractB
+      const tokenA_id = quantityA.split(' ')[1].toLowerCase() + '-' + contractA
+      const tokenB_id = quantityB.split(' ')[1].toLowerCase() + '-' + contractB
 
-    //   // Removing cache to re-generate swap routes
-    //   deleteKeysByPattern(redis, `*routes_*${tokenA_id}*`)
-    //   deleteKeysByPattern(redis, `*routes_*${tokenB_id}*`)
-    // } catch (e) {
-    //   console.error('REMOVE CACHE ROUTES ERR', e, data)
-    // }
+      // Removing cache to re-generate swap routes
+      deleteKeysByPattern(redis, `routes_{chain}-*${tokenA_id}*`)
+      deleteKeysByPattern(redis, `routes_{chain}-*${tokenB_id}*`)
+    } catch (e) {
+      console.error('REMOVE CACHE ROUTES ERR', e, data)
+    }
   }
 
   if (name == 'logswap') {
