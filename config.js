96931ec92bb0168140f0fb005b9d02c9817c9cc6
avral <avral@pm.me>
2025-10-29 18:41:43 +0300

    upd

--- config.js
+++ config.js
@@ -248,7 +248,9 @@ const networks = {
       '54231', 'melaniameme', 'btcreserve', 'fartcoin', 'eosiopowcoin', 'edu.xpr', 'spacedemon',
       'mysticlion', 'zetas', 'starknet.xpr', 'xsatoshi', 'whitebitcoin', 'kibainu', 'kaia', 'erjkerewj',
       'frankcoin', 'vbtn', 'hiphopbank', 'wrappedtoken', 'xu3o5', 'xtok3n', 'xpel',
-      'aquaprotocol', 'tnode', 'projak', 'terramoney', 'protonai', '1gram', 'xprjesus.xpr', 'xrpn', 'acix', 'lambobank'
+      'aquaprotocol', 'tnode', 'projak', 'terramoney', 'protonai', '1gram', 'xprjesus.xpr', 'xrpn', 'acix', 'lambobank',
+      'xasterdex', 'fasa', '51st', 'bfrost', 'xplasma', 'bttt', 'webx', 'sbwy', 'xfund', 'astremn',
+      'vril', 'eurt', 'si44', 'prxq', 'nacuso', 'eveningmoon', '5t4r', 'binaryx', 'cumbank', 'xprn2', 'qxte'
     ],
     CEX_CONTRACTS: [],
 
