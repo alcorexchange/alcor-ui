1344d9bc398ba79e1429ef01f7fe633f5400dc1e
avral <avral@pm.me>
2025-12-28 01:15:04 +0300

    upd

--- config.js
+++ config.js
@@ -253,7 +253,7 @@ const networks = {
       'vril', 'eurt', 'si44', 'prxq', 'nacuso', 'eveningmoon', '5t4r', 'binaryx', 'cumbank', 'xprn2', 'qxte', 'magx',
       'xpot', 'aeonprotocol', 'xmtlchain', 'xnet', 'ytokens', 'tokencreates', 'xrlt', 'xprlite', 'dirtyx', 'bullishdegen',
       'solarspark', 'mindxp', 'xprsl', 'xtoken.xpr', 'xwave', 'xrpay', 'iweb', 'eostoken.xpr', 'xeosio.xpr', 'xiotoken.xpr', 'xmetal.xpr',
-      'x1oken.xpr', 'xtokems.xpr', 'xtok3n.xpr'
+      'x1oken.xpr', 'xtokems.xpr', 'xtok3n.xpr', 'xprn1.xpr', 'xprn2.xpr', 'xprn3.xpr', 'xprn4.xpr'
     ],
     CEX_CONTRACTS: [],
 
